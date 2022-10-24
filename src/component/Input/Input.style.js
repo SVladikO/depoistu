247484d30e40f23927f9e0b5a8170bf6096fe7df
Chernukha21 <Chorniy2008@ukr.net>
2022-10-24 11:55:13 +0300

    Fixed by comments

--- src/component/Input/Input.style.js
+++ src/component/Input/Input.style.js
@@ -2,9 +2,11 @@ import styled from 'styled-components'
 import {THEME} from "../../theme";
 
 export const Input = styled.input`
-  padding: 14px 0 17px 20px;
   background: ${THEME.COLOR.ACCENT2};
+  width: 100%;
+  height: 50px;
   border: none;
+  padding: 0 0 0 20px;
   line-height: 19px;
   border-radius: 3px;
   ::placeholder {
