f7f8cf3ce5fedd782cfb9c6a2f2ca905a0b368f8
Chernukha21 <Chorniy2008@ukr.net>
2022-10-27 16:02:41 +0300

    Deleted input component from develop branch

--- src/page/development/Components.page.jsx
+++ src/page/development/Components.page.jsx
@@ -8,7 +8,6 @@ import {
     SecondaryWithIconButton,
     ThirdButton,
 } from "../../component/Button/Button.style";
-import {Input} from "../../component/Input/Input.style";
 import {ReactComponent as GoogleIcon} from '../../icons/google.svg';
 import {ReactComponent as FacebookIcon} from '../../icons/facebook.svg';
 
@@ -19,7 +18,6 @@ const components = [
     {title: 'SecondaryButton', component: <SecondaryButton>Cancel</SecondaryButton>},
     {title: 'SecondaryWithIconButton', component: <SecondaryWithIconButton><FacebookIcon/>facebook</SecondaryWithIconButton>},
     {title: 'ThirdButton', component: <ThirdButton>Payment</ThirdButton>},
-    {title: 'Input', component: <Input placeholder="Full Name"/>},
 ]
 
 function ComponentsPage() {
