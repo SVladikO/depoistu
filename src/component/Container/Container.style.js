a9aa05a87ceab2b57b3d4b9d20e34b3bb9a4c72d
Chernukha21 <Chorniy2008@ukr.net>
2022-10-27 15:56:31 +0300

    Finished styled container

---
+++ src/component/Container/Container.style.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import {THEME} from "../../theme";
+
+export const StyledContainer = styled.div`
+  margin: 30px 25px 25px 25px;
+  background: ${THEME.COLOR.ACCENT4};
+  width: 100%;
+  min-height: 30px;
+`
