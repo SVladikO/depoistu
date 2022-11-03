e0d095ec9c28da65a9b8bce73bc84f08c17f40de
Chernukha21 <40316815+Chernukha21@users.noreply.github.com>
2022-11-03 15:45:54 +0200

    [PMA-14] Added discount component

---
+++ src/component/Discount/Discount.style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import {THEME} from "../../theme";
+
+
+export const Discount = styled.div`
+  width: 40px;
+  background-color: ${THEME.COLOR.PRIMARY};
+  font-weight: 700;
+  font-size: 12px;
+  padding: 0 3px 0 0;
+  line-height: 16px;
+  border-radius: 8px;
+  color: ${THEME.COLOR.ACCENT4};
+  display: flex;
+  justify-content: center;
+`
