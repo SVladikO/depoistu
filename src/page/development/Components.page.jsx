4a6eeace78079a2c2e0d1684501482910ca1a78e
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2022-11-03 19:46:52 +0200

    PMA-24 Fixed input color

--- src/page/development/Components.page.jsx
+++ src/page/development/Components.page.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {ComponentWrapper, Row} from './Components.style';
+import {Wrapper, Column, ComponentWrapper, Row, ColorCircle} from './Components.style';
 import {
     PrimaryRoundedButton,
     PrimaryWideButton,
@@ -10,40 +10,62 @@ import {
 } from "../../component/Button/Button.style";
 
 import {Discount} from "../../component/Discount/Discount.style";
-import InputWithIcon from "../../component/InputWithIcon/InputWithIcon";
+import Input from "../../component/Input/Input";
 import NavigationHeader from "../../component/TopNavigation/NavigationHeader";
 
 import {ReactComponent as GoogleIcon} from '../../icons/google.svg';
 import {ReactComponent as FacebookIcon} from '../../icons/facebook.svg';
 import {ReactComponent as MailIcon} from '../../icons/mail.svg';
 
+import {THEME} from "../../theme";
 
+const colors = Object.keys(THEME.COLOR).map(key =>
+    ({title: key, component: <ColorCircle bg={THEME.COLOR[key]} />})
+)
 
-const components = [
-    {title: 'PrimaryWithIconButton', component: <PrimaryWithIconButton><GoogleIcon/>Google</PrimaryWithIconButton>},
-    {title: 'PrimaryWideButton', component: <PrimaryWideButton>Sing in</PrimaryWideButton>},
-    {title: 'PrimaryRoundedButton', component: <PrimaryRoundedButton>SIGN IN</PrimaryRoundedButton>},
-    {title: 'SecondaryButton', component: <SecondaryButton>Cancel</SecondaryButton>},
-    {title: 'SecondaryWithIconButton', component: <SecondaryWithIconButton><FacebookIcon/>facebook</SecondaryWithIconButton>},
-    {title: 'ThirdButton', component: <ThirdButton>Payment</ThirdButton>},
-    {title: 'InputWithIcon', component: <InputWithIcon Icon={MailIcon} placeholder={`johndoe@mail.com`}/>},
-    {title: "<NavigationHeader title=\"category\"/>", component: <NavigationHeader title="category"/>},
-    {title: "<NavigationHeader href={\' \'} title=\"category\"/>", component: <NavigationHeader href={' '} title="category"/>},
-    {title: '<Discount>{`-10%`}</Discount>', component: <Discount>{`-10%`}</Discount>}
+// We grouped to have columns
+const columns = [
+    colors,
+    [
+        {title: 'PrimaryWithIconButton', component: <PrimaryWithIconButton><GoogleIcon/>Google</PrimaryWithIconButton>},
+        {title: 'PrimaryWideButton', component: <PrimaryWideButton>Sing in</PrimaryWideButton>},
+        {title: 'PrimaryRoundedButton', component: <PrimaryRoundedButton>SIGN IN</PrimaryRoundedButton>},
+        {title: 'SecondaryButton', component: <SecondaryButton>Cancel</SecondaryButton>},
+        {
+            title: 'SecondaryWithIconButton',
+            component: <SecondaryWithIconButton><FacebookIcon/>facebook</SecondaryWithIconButton>
+        },
+        {title: 'ThirdButton', component: <ThirdButton>Payment</ThirdButton>},
+    ],
+    [
+        {title: 'Input', component: <Input placeholder={`johndoe@mail.com`}/>},
+        {title: 'Input', component: <Input Icon={MailIcon} placeholder={`johndoe@mail.com`}/>},
+        {title: "NavigationHeader", component: <NavigationHeader title="category"/>},
+        {
+            title: "NavigationHeader",
+            component: <NavigationHeader href={' '} title="category"/>
+        },
+        {title: 'Discount', component: <Discount>{`-10%`}</Discount>}
+    ]
 ]
 
 function ComponentsPage() {
     return (
-        <div>
-            {components.map(c =>
-                <Row>
-                    {c.title}
-                    <ComponentWrapper>
-                        {c.component}
-                    </ComponentWrapper>
-                </Row>
-            )}
-        </div>
+        <Wrapper>
+            {columns.map(columnComponents =>
+                <Column>
+                    {columnComponents.map(c =>
+                        <Row>
+                            {c.title}
+                            <ComponentWrapper>
+                                {c.component}
+                            </ComponentWrapper>
+                        </Row>
+                    )
+                    }
+                </Column>)
+            }
+        </Wrapper>
     )
 }
 
--- src/page/development/Components.style.jsx
+++ src/page/development/Components.style.jsx
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+ display: flex;  
+`;
+export const Column = styled.div`
+    border: solid ;
+  width: 400px;
+  padding: 10px;
+  
+`;
+
 export const Row = styled.div`
     padding: 10px;
 `;
@@ -7,4 +17,12 @@ export const ComponentWrapper = styled.div`
   width: 300px;
   border: solid 1px blue;
   margin: 10px 0;
+`;
+
+export const ColorCircle = styled.div`
+  background: ${p => p.bg};
+  border-radius: 40px;
+  width: 80px;
+  height: 80px;
+;
 `;
