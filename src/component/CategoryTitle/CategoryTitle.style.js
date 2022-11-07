a5bfee651b531800b3d1b30f09a3ce6bb6ad0ed7
Chernukha21 <Chorniy2008@ukr.net>
2022-11-07 16:14:23 +0200

    PMA-11-Changed font family

--- src/component/CategoryTitle/CategoryTitle.style.js
+++ src/component/CategoryTitle/CategoryTitle.style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {THEME} from "../../theme";
 
 export const CategoryTitle = styled.div`
-  font-weight: 700;
+  font-family: 'LatoBold';
   font-size: 16px;
   color: ${THEME.COLOR.ACCENT1};
 `
