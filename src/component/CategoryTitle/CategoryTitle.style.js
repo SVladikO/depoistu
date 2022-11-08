b8132fcd309aee827d1781e4a8b27823bf02f004
Chernukha21 <Chorniy2008@ukr.net>
2022-11-08 19:34:34 +0200

    Fixed merge mistake

---
+++ src/component/CategoryTitle/CategoryTitle.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import {THEME} from "../../theme";
+
+export const StyledCategoryTitle = styled.div`
+  font-weight: 700;
+  font-size: 16px;
+  line-height: 19px;
+  color: ${THEME.COLOR.ACCENT1};
+`
