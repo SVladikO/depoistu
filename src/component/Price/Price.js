973b716b4e79df5191a84f23694445e37a844f19
Chernukha21 <Chorniy2008@ukr.net>
2022-11-09 14:44:06 +0200

    Fixed structure

--- src/component/Price/Price.js
+++ src/component/Price/Price.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {Wrapper} from "./Price.style";
 
 const Price = (props) => {
-    const {currency} = props;
     return (
        <Wrapper>
-           {currency}
            {props.children}
        </Wrapper>
     );
