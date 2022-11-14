75866c6ec8cfdb00f2ae424f7fcdd6f677617120
Vlad Serhiychuk <SVladikO@mail.ru>
2022-11-14 19:23:43 +0200

    Revert "[PMA-7] Added rounded button"
    This reverts commit bb1a8fe3facb59f137bb5443ca687ad0ed330d8f.

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,44 +1,12 @@
-import React, {useState} from "react";
-import {Wrapper, InputText, SwitchIconWrapper, CenterWrapper} from "./Input.style";
+import React from "react";
+import {Wrapper, InputText} from "./Input.style";
 
-import {ReactComponent as ShowEyeIcon} from "../../icons/show-eye.svg";
-import {ReactComponent as HideEyeIcon} from "../../icons/hide-eye.svg";
-
-function Input({
-                   Icon,
-                   value,
-                   placeholder,
-                   changeHandler = () => {
-                   },
-                   withSwitcher = false,
-
-               }) {
-
-    const [showData, setShowData] = useState(false);
-
-    const showValue = () => {
-        setShowData(true);
-    }
-    const hideValue = () => setShowData(false);
-
+function Input(props) {
+    const {Icon, placeholder} = props;
     return (
         <Wrapper>
             {Icon && <Icon/>}
-            <InputText
-                type={!withSwitcher ? 'text' : showData ? 'text' : 'password'}
-                value={value}
-                withIcon={!!Icon}
-                onChange={changeHandler}
-                withSwitcher={withSwitcher}
-                placeholder={placeholder}
-            />
-            {withSwitcher &&
-                <SwitchIconWrapper onClick={showData ? hideValue : showValue}>
-                    <CenterWrapper>
-                        {showData ? <HideEyeIcon/> : <ShowEyeIcon/>}
-                    </CenterWrapper>
-                </SwitchIconWrapper>
-            }
+            <InputText placeholder={placeholder} withIcon={!!Icon}/>
         </Wrapper>
     )
 }
--- src/components/Input/Input.style.js
+++ src/components/Input/Input.style.js
@@ -2,32 +2,15 @@ import styled from 'styled-components'
 import {COLOR} from "../../theme";
 
 export const Wrapper = styled.div`
-  position: relative;
-
-  & > svg:first-child {
-    position: absolute;
-    display: block;
-    left: 15px;
-    top: 50%;
-    transform: translateY(-50%);
-  }
+    position: relative;
+    & > svg {
+        position: absolute;
+        display: block;
+        left: 15px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
 `;
-
-export const SwitchIconWrapper = styled.div`
-  position: absolute;
-  display: block;
-  right: 15px;
-  top: 20%;
-`;
-
-export const CenterWrapper = styled.div`
-  height: 30px;
-  width: 30px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 
 export const InputText = styled.input`
   background: ${COLOR.ACCENT2};
@@ -36,14 +19,8 @@ export const InputText = styled.input`
   height: 50px;
   border: none;
   font-size: 16px;
-  padding: ${p => {
-    if (p.withIcon) return '14px 20px 17px 50px'
-    if (p.withSwitcher) return '14px 46px 17px 20px'
-    return '14px 20px 17px 20px'
-  }};
-
-  border-radius: ${p => p.withSwitcher ? '40px' : 0};
-
+  padding: 14px 20px 17px ${p => p.withIcon ? '50px' : '20px'};
+  
   ::placeholder {
     color: ${COLOR.ACCENT1};
   }
