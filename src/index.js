13745b87ac401dd3635c2a2ef7a5c1e9358f0997
Chernukha21 <Chorniy2008@ukr.net>
2022-11-14 17:21:28 +0200

    PMA-25-Added version to browser console

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import {createRoot} from 'react-dom/client';
 import {Provider} from 'react-redux';
 import {store} from './app/store';
 import {BrowserRouter} from "react-router-dom";
-
+import packageInfo from '../package.json';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-
+console.log("version:", packageInfo.version);
 root.render(
     <React.StrictMode>
         <BrowserRouter>
