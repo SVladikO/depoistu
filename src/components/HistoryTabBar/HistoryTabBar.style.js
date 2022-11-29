bd3179e5f08f0f5a8480e61f4ec9a1e0bc4849b4
Chernukha21 <Chorniy2008@ukr.net>
2022-11-29 10:30:50 +0200

    Fixed by remarks

--- src/components/HistoryTabBar/HistoryTabBar.style.js
+++ src/components/HistoryTabBar/HistoryTabBar.style.js
@@ -6,6 +6,9 @@ export const Wrapper =  styled.div`
   justify-content: space-evenly;
   padding: 7px 31px 22px 31px;
   margin: 0;
+  width: 100%;
+  height: 100%;
+  background: ${COLOR.ACCENT4};
 `;
 export const Tab = styled.div`
   width: 33%;
@@ -17,6 +20,6 @@ export const Tab = styled.div`
   &:not(:last-child) {
     margin-right: 6%;
     padding-right: 7%;
-    border-right: 1px solid;
+    border-right: 1px solid ${COLOR.ACCENT1};
   }
 `;
--- src/components/TopNavigation/NavigationHeader.style.js
+++ src/components/TopNavigation/NavigationHeader.style.js
@@ -4,6 +4,8 @@ import {BORDER_RADIUS, COLOR} from "../../utils/theme";
 
 export const BackgroundWrapper = styled.div`
   background: ${COLOR.ACCENT2};
+  height: 100%;
+  border-radius: 0 0 ${BORDER_RADIUS.FOURTH} ${BORDER_RADIUS.FOURTH};
 `;
 export const Wrapper = styled.div`
   
@@ -18,16 +20,17 @@ export const Title = styled.div`
   display: block;
 `
 export const NestedContent = styled.div`
-
+  background: ${COLOR.ACCENT2};
+  width: 100%;
 `;
 export const MainContent = styled.div`
-  min-height: 30px;
+  //min-height: 30px;
   width: 100%;
   display: flex;
   justify-content: center;
   background: ${COLOR.ACCENT4};
   position: relative;
-  border-radius: 0 0 ${BORDER_RADIUS.FOURTH} ${BORDER_RADIUS.FOURTH};
+  
   & > a > svg {
     position: absolute;
     display: block;
