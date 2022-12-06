6d4f2dbd50322eacfc68e149151aa64532763c96
svladiko <vlad.serhiychuk@gmail.com>
2022-12-06 14:25:23 +0200

    fixed ui bug ContentContainer

--- src/components/ContentContainer/ContentContainer.style.js
+++ src/components/ContentContainer/ContentContainer.style.js
@@ -3,7 +3,6 @@ import {BORDER_RADIUS, COLOR} from "../../utils/theme";
 
 export const ContentContainer = styled.div`
   background: ${COLOR.ACCENT4};
-  width: 100%;
   text-align: center;
   padding: 20px 25px 35px 25px;
   border-radius: ${BORDER_RADIUS.SECOND};
