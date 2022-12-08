dc626837a6c496e7d86f524509cf378433430271
Chernukha21 <Chorniy2008@ukr.net>
2022-12-08 15:55:33 +0200

    PMA-50-Fixed problems

--- src/components/UserAccountBar/UserAccountBar.js
+++ src/components/UserAccountBar/UserAccountBar.js
@@ -1,13 +1,15 @@
 import {Link} from "react-router-dom";
 import {ContentWrapper, Wrapper,Avatar,TextContent,Name, Status, Anchor} from "./UserAccountBar.style";
 import {ReactComponent as RightAnchor} from "../../icons/right-anchor.svg";
+import avatar from '../../icons/avatar.svg';
+
 
 const UserAccountBar = ({ fullName, status, href}) => {
     return (
         <Link to={href}>
             <Wrapper>
                 <ContentWrapper>
-                    <Avatar src="https://via.placeholder.com/60" alt="user_photo"/>
+                    <Avatar src={avatar} alt="user_photo"/>
                     <TextContent>
                         <Name>{fullName}</Name>
                         <Status>{status}</Status>
