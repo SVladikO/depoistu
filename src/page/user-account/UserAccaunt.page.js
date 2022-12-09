c11af6b32ed4e116e830897d8f69c7a3f66515bf
svladiko <vlad.serhiychuk@gmail.com>
2022-12-09 10:22:45 +0200

    fc

--- src/page/user-account/UserAccaunt.page.js
+++ src/page/user-account/UserAccaunt.page.js
@@ -1,13 +1,7 @@
 import React from 'react';
 
-// import { A } from "./UserAccaunt.style";
-
-import {ReactComponent as GoogleIcon} from '../../icons/google.svg';
-import {ReactComponent as FacebookIcon} from '../../icons/facebook.svg';
-import {ReactComponent as MailIcon} from '../../icons/mail.svg';
 import {ReactComponent as LockIcon} from '../../icons/lock.svg';
 import {ReactComponent as LogOutIcon} from "../../icons/logout.svg";
-import {ReactComponent as SandwichIcon} from '../../icons/sandwich.svg';
 import {ReactComponent as LanguageIcon} from "../../icons/language.svg";
 
 import {
@@ -16,11 +10,6 @@ import {
     UserAccountGroup,
     UserOptionGroup,
 } from '../../components'
-// import {ReactComponent as LockIcon} from "../../icons/lock.svg";
-// import {ReactComponent as LogOutIcon} from "../../icons/logout.svg";
-// import {ReactComponent as LanguageIcon} from "../../icons/language.svg";
-
-// import {ReactComponent as LogoIcon} from "../../icons/logo.svg";
 
 const UserAccountPage = () => {
     return (
