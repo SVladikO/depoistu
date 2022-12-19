34daf024386d2f4d3b717091ff7df1c1f5cb4fb1
svladiko <vlad.serhiychuk@gmail.com>
2022-12-19 14:50:54 +0200

    [PMA-69] Changed set port approach

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const app = express()
-const port = 3000;
-app.get('/', function (req, res) {
+const server = express()
+server.get('/', function (req, res) {
     res.send('Hello World')
 })
 
-console.log('Project was run on ', port)
-app.listen(port)
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = server;
