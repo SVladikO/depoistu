eb22deceda7eafe13ccbb27fafcb9e50633f4f49
Vlad Serhiychuk <SVladikO@mail.ru>
2022-12-23 18:25:27 +0200

    [PMA-79] Show category on remote and local be (#61)

--- api/index.js
+++ api/index.js
@@ -1,36 +1,29 @@
 const express = require('express')
 const server = express()
 const dbConfig = require('./db/config')
+const QUERY = require('./db/query')
 const {Pool} = require('pg');
 
 const pool = new Pool(dbConfig);
 
 server.get('/', function (req, res) {
-    const query = {
-        // give the query a unique name
-        name: 'fetch-menu-items',
-        text: 'SELECT * FROM root.menu_item',
-    }
+    pool.connect((err) => {
+        if (err) {
+            console.error('connection error', err.stack)
+        } else {
+            console.log('connected')
+        }
+    })
 
-    pool
-        .connect((err) => {
-            if (err) {
-                console.error('connection error', err.stack)
-            } else {
-                console.log('connected')
-            }
-        })
-
-    pool
-        .query(query.text)
-        .then(responce => {
-            console.log(1111111111)
-            console.log(responce.rows)
-            res.send(responce.rows)
+    pool.query(QUERY.MENU_ITEM.SELECT_ALL)
+        .then(r => {
+            console.log('DB request: ', QUERY.MENU_ITEM.SELECT_ALL)
+            console.log('DB response: ', r.rows)
+            res.send(r.rows)
         })
         .catch(e => {
-            console.error(e.stack)
-            res.send(e.stack)
+            console.error('DB error: ', e.stack)
+            res.send('DB error: ' + e.stack)
         })
 })
 
