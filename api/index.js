2f4c01e07ecdff894768e6e035997eaeeded2453
svladiko <vlad.serhiychuk@gmail.com>
2022-12-23 17:05:27 +0200

    [PMA-80] connect to db from be

--- api/index.js
+++ api/index.js
@@ -1,7 +1,47 @@
 const express = require('express')
+const tls = require('node:tls');
 const server = express()
+// const client = require('./db/connection')
+const { Client } = require('pg');
+
+const client = new Client({
+    user: 'pizza_mobile_db_yjul_user',
+    database: 'pizza_mobile_db_yjul',
+    host: 'dpg-ceia2u2rrk07uhbp15fg-a.frankfurt-postgres.render.com',
+    port: 5432,
+    password: 'oA3OlpU9RIhBXnY1sSi36diIv53qGxTq',
+    ssl: tls,
+})
+
+
 server.get('/', function (req, res) {
-    res.send('Hello World')
+    const query = {
+        // give the query a unique name
+        name: 'fetch-menu-items',
+        text: 'SELECT * FROM root.menu_item',
+    }
+
+    client
+        .connect((err) => {
+            if (err) {
+                console.error('connection error', err.stack)
+            } else {
+                console.log('connected')
+            }
+        })
+        client
+        .query(query.text)
+        .then(responce => {
+            console.log(1111111111)
+            console.log(responce.rows)
+            client.end()
+            res.send(responce.rows)
+        })
+        .catch(e => {
+            console.error(e.stack)
+            res.send(e.stack)
+            client.end()
+        })
 })
 
 const PORT = process.env.PORT || 5000;
