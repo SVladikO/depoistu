0bc9dbd13845dcf882436a0057dfd5dadd5af095
svladiko <vlad.serhiychuk@gmail.com>
2022-12-24 11:55:44 +0200

    [PMA-63] Added sub category page

--- src/page/sub-category/SubCategory.page.js
+++ src/page/sub-category/SubCategory.page.js
@@ -11,7 +11,7 @@ const SubCategoryPage = () => {
     return (
         <Content>
             <Flex>
-                {[1, 3, 4, 5, 6].map(c => <ProductCard
+                {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].map(c => <ProductCard
                     data={{
                         image: 'https://www.freeiconspng.com/thumbs/pizza-png/pizza-png-15.png',
                         discont: '-10',
