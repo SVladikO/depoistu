91d60a1ef9e81b4ae9dd0cc0c97ac34437d1c6b6
svladiko <vlad.serhiychuk@gmail.com>
2023-01-18 22:06:38 +0200

    [PMA-119] Created new menu_item component to show in sub category

---
+++ src/components/Flex/Flex.style.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Flex = styled.div`
+  display: flex;
+  ${p => p.width && `width: ${p.width}`};
+  flex-direction: ${p => p.flexDirection || 'row'};
+  justify-content: ${p => p.justifyContent || 'norman'};
+  ${p => p.margin ? `margin: ${p.margin}` : ''};
+`;
