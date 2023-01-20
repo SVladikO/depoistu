be4ac1b3a0fa925d8bf5ef3365069b9b652c37e1
Chernukha21 <Chorniy2008@ukr.net>
2023-01-20 16:16:20 +0200

    PMA-65-Changed history tab bar

--- src/components/HistoryTabBar/HistoryTabBar.js
+++ src/components/HistoryTabBar/HistoryTabBar.js
@@ -2,15 +2,15 @@ import {Tab, Wrapper} from "./HistoryTabBar.style";
 import {useState} from "react";
 
 const HistoryTabBar = () => {
-    const tabs = ['Completed', 'Upcoming', 'Cancelled'];
-    const changeTab = (i) => {setSelectedTab(tabs[i])};
-    const [selectedTab, setSelectedTab] = useState(tabs[0]);
+    const tabTitles = ['Completed', 'Upcoming', 'Cancelled'];
+    const [selectedTabTitle, setSelectedTabTitle] = useState(tabTitles[0]);
+
     return (
         <Wrapper>
-            {tabs.map((tab, index) => <Tab
-                key={index}
-                onClick={() => changeTab(index)}
-                active={selectedTab === tabs[index]}>{tab}</Tab>)}
+            {tabTitles.map(title => <Tab
+                key={title}
+                onClick={() => setSelectedTabTitle(title)}
+                active={selectedTabTitle === title}>{title}</Tab>)}
         </Wrapper>
     );
 };
