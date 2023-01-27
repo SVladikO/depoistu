112cc1c3d578fc7beb839e8bec1a4c3933a809b3
Vlad Serhiychuk <SVladikO@mail.ru>
2023-01-27 14:16:30 +0200

    Revert "[PMA-130] Changed design in menuItem"
    This reverts commit 328128248e9fadede084335333d8ccbd104340d7.

--- src/components/MenuItem/MenuItem.style.js
+++ src/components/MenuItem/MenuItem.style.js
@@ -12,52 +12,42 @@ export const Wrapper = styled.div`
   line-height: 19px;
   background: ${COLOR.ACCENT4};
   border-radius: ${BORDER_RADIUS.SECOND};
-  svg{
-    width: 25px;
-    height: 25px;
-  }
 `
 
 export const FoodImage = styled.img`
-  min-width: 90px;
-  min-height: 90px;
-  max-width: 90px;
-  max-height: 90px;
+  min-width: 60px;
+  min-height: 60px;
+  max-width: 60px;
+  max-height: 60px;
   background: ${COLOR.ACCENT2};
   border-radius: ${BORDER_RADIUS.CIRCLE};
   border: solid 1px ${COLOR.ACCENT4};
-  margin: 0 10px 0 0;
 `;
 
 export const Title = styled.div`
   font-weight: 700;
-  font-size: 18px;
-  line-height: 24px;
+  font-size: 16px;
+  line-height: 19px;
   color: ${COLOR.ACCENT1};
   margin: 0 0 4px;
 `;
 
 export const Description = styled.div`
-  //width: 250px;
-  font-weight: 400;
-  font-size: 16px;
-  line-height: 19px;
+  width: 250px;
+  font-size: 14px;
+  font-weight: 100;
   margin: 4px 0 28px 0;
 `;
-export const InvisibleDivider = styled.div`
-  opacity: 0;
-  height: 10px;
-`;
+
 export const AdditionalDetails = styled.div`
   display: flex;
   align-items: center;
+  font-size: 14px;
   font-weight: 500;
-  font-size: 16px;
-  line-height: 19px;
-  color: ${COLOR.ACCENT5};
+  color: ${COLOR.ACCENT1};
 
   & > svg {
-    fill: #b5b5b5;
+    fill: ${COLOR.ACCENT1};
     height: 14px;
     width: 14px;
     margin: 0 2px 0 0;
