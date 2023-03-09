20ca3450cd5a3b703e58341586f93983390d41dd
svladiko <vlad.serhiychuk@gmail.com>
2023-03-09 10:50:59 +0200

    f

--- src/page/development/Components.page.jsx
+++ src/page/development/Components.page.jsx
@@ -71,9 +71,11 @@ const columns = [
 
     ],
     [
-        { title: 'Institution', component: <Institution company={{images: ['https://topclub.ua/uploads/images/places/371-200/_0H8l4_aCp-LNAn-Z-0IzeGKpoRn2Qd-.jpg', 'https://afisha.bigmir.net/i/49/23/90/7/4923907/gallery/a9f2cb111d1abe2b2b8fe5b46db2ac54-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg', 'https://afisha.bigmir.net/i/23/51/30/9/2351309/gallery/15b8175dc297f8a58d9de22e77b7b256-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg'],
-                name: 'Domono', city: 'Kyiv', street: 'Davidusk 15.',}}/>},
-
+        {
+          title: 'Institution',
+          component: <Institution company={{images: ['https://topclub.ua/uploads/images/places/371-200/_0H8l4_aCp-LNAn-Z-0IzeGKpoRn2Qd-.jpg', 'https://afisha.bigmir.net/i/49/23/90/7/4923907/gallery/a9f2cb111d1abe2b2b8fe5b46db2ac54-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg', 'https://afisha.bigmir.net/i/23/51/30/9/2351309/gallery/15b8175dc297f8a58d9de22e77b7b256-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg'],name: 'Domono', city: 'Kyiv', street: 'Davidusk 15.',}}/>
+        },
+        {title: 'CategoryMenuRow', component: <CategoryMenuRow categories={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]} />},
         {
             title: 'MenuItem',
             component:
@@ -117,7 +119,6 @@ const columns = [
 
     ],
     [
-        {title: 'CategoryMenuRow', component: <CategoryMenuRow categories={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]} />},
         {title: 'PopupImage', component: <ImagePopupContent imageUrl="https://raw.githubusercontent.com/SVladikO/testApp/master/images/4_cheese.jpg" />},
         {title: 'PopupIntro', component: <PopupIntroContent />},
         {title: 'EmptyBasket', component:
