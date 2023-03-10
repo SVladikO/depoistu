d3bda6e9c9e4ac9eba7faca740dc6ddeff0e831e
Chernukha21 <Chorniy2008@ukr.net>
2023-03-10 12:55:44 +0200

    [PMA-160]-Finished search by city functionality

--- src/page/search/Search.page.js
+++ src/page/search/Search.page.js
@@ -1,26 +1,26 @@
 import React, {useEffect, useState} from "react";
-import {InputWrapper} from "./Search.page.style";
+import {InputWrapper, Warning} from "./Search.page.style";
 import {Input} from "../../components";
 import {ReactComponent as LocationIcon} from "../../icons/map_point.svg";
 import {showCityPopup} from "../../features/cityPopup/cityPopupSlice";
 import {useDispatch, useSelector} from "react-redux";
-import utf8 from "utf8";
-
+import {Institution} from "../../components";
 
 const SearchPage = () => {
     const dispatch = useDispatch();
     const selectedCity = useSelector(state => state.cityPopup.selectedCity);
     const selectedRegion = useSelector(state => state.cityPopup.selectedRegion);
     const [companies, setCompanies] = useState([]);
+    const url = `https://pizza-mobile-api.herokuapp.com/companies/${selectedCity}`;
+    const warning = <Warning>There is no installations in current city</Warning>;
+
 
     useEffect(() => {
-        const url = encodeURI(`https://pizza-mobile-api.herokuapp.com/companies/?x=${selectedCity.toLowerCase()}`);
-        let myHeaders = new Headers();
-        myHeaders.append('Content-Type','text/plain; charset=UTF-8');
-         fetch(url,myHeaders)
+        fetch(decodeURIComponent(url),)
             .then(response => response.json())
             .then(data => setCompanies(data));
     }, [selectedCity]);
+
 
     return (
         <>
@@ -31,7 +31,7 @@ const SearchPage = () => {
                     value={selectedCity ? `${selectedCity}, ${selectedRegion} обл` : ""}
                 />
             </InputWrapper>
-            {companies.map(company => <div key={company.ID}>{company.name}</div>)}
+            {companies.length === 0 ? warning : selectedCity && companies.map(company => <Institution  key={company.ID} company={company}/>)}
         </>
     );
 };
