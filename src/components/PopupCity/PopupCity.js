2deb5a0e8ef9c5b89eabc944ab6d4461738b06ca
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-03-13 15:16:03 +0200

    Fixed 4

--- src/components/PopupCity/PopupCity.js
+++ src/components/PopupCity/PopupCity.js
@@ -49,8 +49,8 @@ export const CityPopupContent = () => {
 
     return (
         <Wrapper
-            onTouchStart={prevent}
-            onMouseDown={prevent}
+            onMouseUp={prevent}
+            onTouchEnd={prevent}
         >
             <ContentContainer style={style}>
                 {
