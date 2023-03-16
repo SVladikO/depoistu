20607559c2d841a462c6fd8d4b54f90cbb02e40b
Chernukha21 <40316815+Chernukha21@users.noreply.github.com>
2023-03-16 20:27:17 +0200

    [PMA-160] Added search by city functionality. Request to BE.

--- src/components/PopupCity/PopupCity.js
+++ src/components/PopupCity/PopupCity.js
@@ -2,12 +2,19 @@ import {useDispatch, useSelector} from "react-redux";
 
 import {BackButtonWrapper, BackButtonInnerWrapper, Wrapper, CitiesWrapper, TopHider, BottomHider} from "./PopupCity.style"
 import {getRegions} from "../../utils/utils";
-import {cities} from "../../features/cityPopup/cities";
+import {cities as citiesJSON} from "../../features/cityPopup/cities";
 import {InvisibleWrapper} from '../PopupInvisiableWrapper/PopupInvisiableWrapper.style'
-import {hideCityPopup, setSelectedRegion, showRegions, setSelectedCity} from "../../features/cityPopup/cityPopupSlice";
+import {
+    hideCityPopup,
+    setSelectedRegion,
+    setCities,
+    setSelectedCity,
+    setIsRegion
+} from "../../features/cityPopup/cityPopupSlice";
 import {ContentContainer, SettingMenuRow} from '../index'
-
 import {ReactComponent as BackIcon} from "../../icons/back.svg";
+
+const enableScrollOnBody = () => document.body.style.position = 'relative';
 
 const PopupCity = () => {
     const isVisiblePopup = useSelector(state => state.cityPopup.isVisible);
@@ -19,8 +26,8 @@ const PopupCity = () => {
 
     return (
         <InvisibleWrapper onClick={() => {
-            dispatch(hideCityPopup())
-            document.body.style.position = 'relative';
+            dispatch(hideCityPopup());
+            enableScrollOnBody();
         }}>
             <CityPopupContent/>
         </InvisibleWrapper>
@@ -30,7 +37,7 @@ const PopupCity = () => {
 export const CityPopupContent = () => {
     const dispatch = useDispatch();
 
-    let city = useSelector(state => state.cityPopup.city)
+    let cities = useSelector(state => state.cityPopup.cities)
     const isRegion = useSelector(state => state.cityPopup.isRegion);
 
     const style = {
@@ -45,38 +52,49 @@ export const CityPopupContent = () => {
             }}
             style={style}
         >
-            {isRegion && <TopHider />}
-            {
-                !isRegion &&
-                <BackButtonWrapper onClick={() => dispatch(showRegions(getRegions(cities)))}>
-                    <BackButtonInnerWrapper>
-                        <BackIcon/>
-                        Back
-                    </BackButtonInnerWrapper>
-                </BackButtonWrapper>
-            }
-            <CitiesWrapper style={{height: isRegion ? '100%' : '92%'}} className="cities">
-                {city.map((c, i) =>
-                    <SettingMenuRow
-                        key={i.toString()}
-                        changeHandler={
-                            () => {
-                                if (isRegion) {
-                                    dispatch(setSelectedRegion(c))
-                                    document.getElementsByClassName('cities')[0].scrollTo(0, 0);
-                                    return;
+            <ContentContainer onClick={e => e.stopPropagation()} style={style}>
+                {
+                    !isRegion
+                    &&
+                    <BackButtonWrapper>
+                        <BackButtonInnerWrapper onClick={() => {
+                            dispatch(setCities(getRegions(citiesJSON)))
+                            dispatch(setIsRegion(true))
+                            dispatch(setSelectedRegion(""))
+                            dispatch(setSelectedCity(""))
+                        }}>
+                            <BackIcon/>
+                            Back
+                        </BackButtonInnerWrapper>
+                    </BackButtonWrapper>
+                }
+                <CitiesWrapper style={{height: isRegion ? '100%' : '92%'}} className="cities">
+                    {cities.map((c, i) =>
+                        <SettingMenuRow
+                            key={i.toString()}
+                            changeHandler={
+                                () => {
+                                    if (isRegion) {
+                                        dispatch(setSelectedRegion(c))
+                                        dispatch(setCities(citiesJSON[c]))
+                                        dispatch(setIsRegion(false))
+                                        document.getElementsByClassName('cities')[0].scrollTo(0, 0);
+                                        return;
+                                    }
+                                    dispatch(setSelectedCity(c));
+                                    dispatch(hideCityPopup());
+
+                                    enableScrollOnBody();
+                                    // setIsVisibleCity(false);
                                 }
-                                dispatch(setSelectedCity(c));
-                                // setIsVisibleCity(false);
                             }
-                        }
-                        title={isRegion ? c + ' область' : c}
-                        label=""
-                        style={{margin: 0, padding: '0 0 20px'}}
-                    />
-                )}
-            </CitiesWrapper>
-            <BottomHider />
+                            title={isRegion ? c + ' область' : c}
+                            label=""
+                            style={{margin: 0, padding: '0 0 20px'}}
+                        />
+                    )}
+                </CitiesWrapper>
+            </ContentContainer>
         </Wrapper>
     )
 };
--- src/page/search/Search.page.js
+++ src/page/search/Search.page.js
@@ -1,12 +1,41 @@
-import {PInput, ContentContainer} from "../../components";
-import {ReactComponent as LocationIcon} from "../../icons/map_point.svg";
-import {showCityPopup} from "../../features/cityPopup/cityPopupSlice";
+import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
+
+import {Warning} from "./Search.page.style";
+
+import {ReactComponent as LocationIcon} from "../../icons/map_point.svg";
+import {PInput, ContentContainer, Institution} from "../../components";
+import {showCityPopup} from "../../features/cityPopup/cityPopupSlice";
 
 const SearchPage = () => {
     const dispatch = useDispatch();
     const selectedCity = useSelector(state => state.cityPopup.selectedCity);
     const selectedRegion = useSelector(state => state.cityPopup.selectedRegion);
+    const [companies, setCompanies] = useState([]);
+    const url = `https://pizza-mobile-api.herokuapp.com/companies/${selectedCity}`;
+    const [warning, setWarning] = useState('');
+    const warningMessage = 'There is no installations in current city';
+
+    const showWarning = () => {
+        setWarning(warningMessage)
+    }
+
+    useEffect(() => {
+        if (selectedCity === '') {
+            return;
+        }
+        fetch(decodeURIComponent(url),)
+            .then(response => response.json())
+            .then(data => {
+                if (data.length === 0) {
+                    showWarning();
+                }
+                setCompanies(data)
+            }).catch(e => {
+            showWarning();
+        })
+    }, [selectedCity]);
+
 
     const openCityPopup = () => {
         dispatch(showCityPopup());
@@ -14,15 +43,19 @@ const SearchPage = () => {
     }
 
     return (
-        <ContentContainer>
-            <PInput
-                handleClick={openCityPopup}
-                withIcon
-                Icon={LocationIcon}
-            >
-                {selectedCity ? `${selectedCity}, ${selectedRegion} обл` : ""}
-            </PInput>
-        </ContentContainer>
+        <>
+            <ContentContainer>
+                <PInput
+                    handleClick={openCityPopup}
+                    withIcon
+                    Icon={LocationIcon}
+                >
+                    {selectedCity ? `${selectedCity}, ${selectedRegion} обл` : ""}
+                </PInput>
+            </ContentContainer>
+            {companies.length === 0 ? <Warning>{warning}</Warning> : selectedCity && companies.map(company =>
+                <Institution key={company.ID} company={company}/>)}
+        </>
     );
 };
 
