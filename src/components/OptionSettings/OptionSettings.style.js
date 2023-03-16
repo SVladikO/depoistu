941eb022e43c0d85f042124e29ee3cb5135a52c5
svladiko <vlad.serhiychuk@gmail.com>
2023-03-16 21:37:11 +0200

    [PMA-199] Add business group to Setting.page.js

--- src/components/OptionSettings/OptionSettings.style.js
+++ src/components/OptionSettings/OptionSettings.style.js
@@ -5,7 +5,7 @@ import {hexToRgbA} from "../../utils/utils";
 
 export const Wrapper = styled.div`
   background: ${COLOR.ACCENT4};  
-  border-radius: 0 0 ${BORDER_RADIUS.SECOND} ${BORDER_RADIUS.SECOND};
+   ${p => p.noBorder ? "" : `border-radius: 0 0 ${BORDER_RADIUS.SECOND} ${BORDER_RADIUS.SECOND};`};
 `;
 
 export const GroupTitle = styled.div`
