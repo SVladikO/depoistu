8b3063dac84b7ccb4cf62989165417791efa3786
Vlad Serhiychuk <SVladikO@mail.ru>
2023-03-16 21:12:13 +0200

    [PMA-198] Refactored page naming.

--- src/page/result/Result.page.js
+++ src/page/result/Result.page.js
@@ -1,16 +1,16 @@
 import {Institution, CategoryMenuRow, MenuItem} from "../../components";
-import {Wrapper,Divider} from "./Search.page1.style";
+import {Wrapper, Divider} from "./Result.page.style";
 
-const company= {
-        PHOTOS: `https://topclub.ua/uploads/images/places/371-200/_0H8l4_aCp-LNAn-Z-0IzeGKpoRn2Qd-.jpg,
+const company = {
+    PHOTOS: `https://topclub.ua/uploads/images/places/371-200/_0H8l4_aCp-LNAn-Z-0IzeGKpoRn2Qd-.jpg,
                  https://afisha.bigmir.net/i/49/23/90/7/4923907/gallery/a9f2cb111d1abe2b2b8fe5b46db2ac54-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg,
                  https://afisha.bigmir.net/i/23/51/30/9/2351309/gallery/15b8175dc297f8a58d9de22e77b7b256-quality_75Xresize_1Xallow_enlarge_0Xw_800Xh_0.jpg`,
-        NAME: 'Domono',
-        CITY: 'Kyiv',
-        STREET: 'Davidusk 15.'
-    };
-const categories=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const item={
+    NAME: 'Domono',
+    CITY: 'Kyiv',
+    STREET: 'Davidusk 15.'
+};
+const categories = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+const item = {
     id: 10,
     name: '4 Cheese',
     description: 'spicy , tomato, sauce, chili, mozzarella, spicy , tomato, sauce, chili, mozzarella',
@@ -20,7 +20,7 @@ const item={
     size: 150,
 }
 
-const SearchPage1 = () => {
+const ResultPage = () => {
 
     return (
         <Wrapper>
@@ -35,4 +35,4 @@ const SearchPage1 = () => {
     );
 };
 
-export default SearchPage1;
+export default ResultPage;
