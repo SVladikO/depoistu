ac3eb59eaa750e7b658a72c4abb6086486741883
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-04-01 20:10:02 +0300

    f

--- src/components/PopupCity/PopupCity.style.js
+++ src/components/PopupCity/PopupCity.style.js
@@ -25,11 +25,16 @@ export const Wrapper = styled.div`
 `;
 
 export const BackButtonWrapper = styled.div`
+  z-index: 1;
   width: 100%;
   padding: 10px 10px 0;
   margin: 0;
   height: 43px;
   box-shadow: 0 2px 4px 0 rgb(0 0 0 / 11%);
+  
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 export const BackButtonInnerWrapper = styled.div`
@@ -59,16 +64,4 @@ export const CitiesWrapper = styled.div`
   & > div:last-child {
     padding: 0;
   }
-`;
-
-export const TopHider = styled.div`
-  height: 10px;
-  width: 100%;
-  background: ${COLOR.ACCENT4};
-`;
-
-export const BottomHider = styled.div`
-  height: 10px;
-  width: 100%;
-  background: ${COLOR.ACCENT4};
 `;
