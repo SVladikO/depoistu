1d5d7e4afb5f91b0fbd76a3438058875ef63da6d
Chernukha21 <40316815+Chernukha21@users.noreply.github.com>
2023-04-04 23:06:36 +0300

    [PMA-253] Added loading for edit menu edit company.

--- src/page/edit-company-list/EditCompanyList.page.js
+++ src/page/edit-company-list/EditCompanyList.page.js
@@ -1,15 +1,17 @@
-import {Wrapper,EditBar,EditButton} from "./EditCompanyList.style";
-import {Company} from "../../components";
+import {Wrapper, EditBar, EditButton} from "./EditCompanyList.style";
+import {Company, Notification} from "../../components";
 import {PrimaryWideButton} from "../../components";
 import {ReactComponent as DeleteIcon} from "../../icons/white_busket.svg";
 import {ReactComponent as EditIcon} from "../../icons/edit.svg";
 import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {useSelector} from "react-redux";
 import {LocalStorage} from "../../utils/utils";
 import {useLocalStorageFetch} from "../../utils/hook";
 import {BE_API, ROUTER} from "../../utils/config";
-import {Link} from "react-router-dom";
 
 const EditCompanyListPage = () => {
+    const isLoading = useSelector(state => state.request.value.isLoading);
     const [customer] = useState(LocalStorage.getGuest());
     const [customerCompanies] = useLocalStorageFetch(
         'customerCompanies',
@@ -17,7 +19,9 @@ const EditCompanyListPage = () => {
         BE_API.GET_COMPANIES_BY_CUSTOMER_ID(customer.ID)
     );
 
-    console.log(customerCompanies);
+    if (isLoading) {
+        return <Notification.Loading/>;
+    }
 
     return (
         <Wrapper>
