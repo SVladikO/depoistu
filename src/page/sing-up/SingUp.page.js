af8912547a939f49ceacd103a289922cc1ba47cd
Chernukha21 <Chorniy2008@ukr.net>
2023-04-21 19:20:36 +0300

    [PMA-291]-Validated SingUp page

--- src/page/sing-up/SingUp.page.js
+++ src/page/sing-up/SingUp.page.js
@@ -4,7 +4,40 @@ import NavigationLabelHref from "../../components/NavigationLabelHref/Navigation
 import {ROUTER} from '../../utils/config';
 import {resolveTranslation} from "../../utils/utils";
 import {useCallback, useState} from "react";
+import {Formik} from "formik";
+import * as Yup from 'yup';
 
+
+
+const SignUpSchema = Yup.object().shape({
+    name: Yup.string()
+        .min(2, "Too Short!")
+        .max(50, "Too Long!")
+        .required("Name is required"),
+    password: Yup.string()
+        .min(6, 'Too Short! Min length 6')
+        .max(12, 'Too Long! Max length 12')
+        .required('Required'),
+    email: Yup.string()
+        .email('Invalid email')
+        .max(30, 'Too Long! Max length 30')
+        .required('Required'),
+    phoneNumber: Yup.string()
+        .required("Phone number is required")
+        .matches(
+            /^\+[0-9]{3}\s\((\d+)\)-\d{3}-\d{2}-\d{2}/g,
+            "Invalid phone number"
+        ),
+    newPassword: Yup.string()
+        .max(30, 'Too Long! Max length 30')
+        .required('Required'),
+    confirmedPassword: Yup.string()
+        .test('passwords-match', 'Passwords must match', function (value) {
+            return this.parent.newPassword === value
+        }),
+    termsAndConditions: Yup.bool()
+        .oneOf([true], 'You need to accept the terms and conditions'),
+});
 
 const SingUpPage = () => {
     const [name, setName] = useState('');
@@ -33,56 +66,87 @@ const SingUpPage = () => {
 
     return (
         <>
-            <Container>
-                <Title>{resolveTranslation("PAGE.SING_UP.CREATE_ACCOUNT")}</Title>
-                <Label>{resolveTranslation("PAGE.SING_UP.LABEL.USER_NAME")}</Label>
-                <Input
-                    withCleaner
-                    value={name}
-                    changeHandler={nameChangeHandler}
-                    clearHandler={nameClearHandler}
-                />
-                <Label>{resolveTranslation("PAGE.SING_UP.LABEL.PHONE")}</Label>
-                <Input
-                    withCleaner
-                    value={phone}
-                    changeHandler={phoneChangeHandler}
-                    clearHandler={phoneClearHandler}
-                />
-                <Label>{resolveTranslation("PAGE.SING_UP.LABEL.EMAIL")}</Label>
-                <Input
-                    withCleaner
-                    type={'email'}
-                    value={email}
-                    changeHandler={emailChangeHandler}
-                    clearHandler={emailClearHandler}
-                />
-                <Label>{resolveTranslation("PAGE.SING_UP.LABEL.PASS")}</Label>
-                <Input
-                    withSwitcher
-                    type={newPasswordType}
-                    value={newPassword}
-                    changeHandler={newPasswordChangeHandler}
-                    switchHandler={newPasswordSwitchHandler}
-                />
-                <Label>{resolveTranslation("PAGE.SING_UP.LABEL.CONFIRM_PASS")}</Label>
-                <Input
-                    withSwitcher
-                    type={confirmedPasswordType}
-                    value={confirmedPassword}
-                    changeHandler={confirmedPasswordChangeHandler}
-                    switchHandler={confirmedPasswordSwitchHandler}
-                />
-                <CheckBoxWithLabel label={resolveTranslation("PAGE.SING_UP.CHECKBOX_CONFIRM_TERMS")}/>
-            </Container>
-            <Wrapper>
-                <NavigationLabelHref
-                    hrefTitle={resolveTranslation("PAGE.SING_IN.TOP_TITLE")}
-                    to={`${ROUTER.SING_IN.URL}`}
-                    label={resolveTranslation("PAGE.SIGN_IN.ACCOUNT_CONFIRMATION")}
-                />
-            </Wrapper>
-            <PrimaryButton>{resolveTranslation("PAGE.SING_UP.TOP_TITLE")}</PrimaryButton>
+            <Formik
+                initialValues={{
+                    name: '',
+                    email: '',
+                    password: '',
+                    newPassword: '',
+                    confirmedPassword: '',
+                    phoneNumber: '',
+                    termsAndConditions: false
+                }}
+                validationSchema={SignUpSchema}
+                onSubmit={values => {
+                    console.log(values);
+                }}
+            >
+                {({values, handleSubmit, handleChange, errors, touched}) => (
+                    <form onSubmit={handleSubmit}>
+                        <Container>
+                            <Title>{resolveTranslation("PAGE.SING_UP.CREATE_ACCOUNT")}</Title>
+                            <Label>{resolveTranslation("PAGE.SING_UP.LABEL.USER_NAME")}</Label>
+                            <Input
+                                withCleaner
+                                name="name"
+                                value={values.name}
+                                changeHandler={handleChange}
+                                clearHandler={nameClearHandler}
+                            />
+                            {errors.name && touched.name && <div>{errors.name}</div>}
+                            <Label>{resolveTranslation("PAGE.SING_UP.LABEL.PHONE")}</Label>
+                            <Input
+                                withCleaner
+                                name="phone"
+                                value={values.phone}
+                                changeHandler={handleChange}
+                                clearHandler={phoneClearHandler}
+                            />
+                            {errors.phoneNumber && touched.phoneNumber && <div>{errors.phoneNumber}</div>}
+                            <Label>{resolveTranslation("PAGE.SING_UP.LABEL.EMAIL")}</Label>
+                            <Input
+                                withCleaner
+                                type={'email'}
+                                name="email"
+                                value={values.email}
+                                changeHandler={handleChange}
+                                clearHandler={emailClearHandler}
+                            />
+                            {errors.email && touched.email && <div>{errors.email}</div>}
+                            <Label>{resolveTranslation("PAGE.SING_UP.LABEL.PASS")}</Label>
+                            <Input
+                                withSwitcher
+                                name="newPassword"
+                                type={newPasswordType}
+                                value={values.newPassword}
+                                changeHandler={handleChange}
+                                switchHandler={newPasswordSwitchHandler}
+                            />
+                            {errors.newPassword && touched.newPassword && <div>{errors.newPassword}</div>}
+                            <Label>{resolveTranslation("PAGE.SING_UP.LABEL.CONFIRM_PASS")}</Label>
+                            <Input
+                                withSwitcher
+                                type={confirmedPasswordType}
+                                value={values.confirmedPassword}
+                                name="confirmedPassword"
+                                changeHandler={handleChange}
+                                switchHandler={confirmedPasswordSwitchHandler}
+                            />
+                            {errors.confirmedPassword && touched.confirmedPassword && <div>{errors.confirmedPassword}</div>}
+                            <CheckBoxWithLabel name="termsAndConditions" label={resolveTranslation("PAGE.SING_UP.CHECKBOX_CONFIRM_TERMS")}/>
+                            {errors.termsAndConditions && touched.termsAndConditions && <div>{errors.termsAndConditions}</div>}
+                        </Container>
+                        <Wrapper>
+                            <NavigationLabelHref
+                                hrefTitle={resolveTranslation("PAGE.SING_IN.TOP_TITLE")}
+                                to={`${ROUTER.SING_IN.URL}`}
+                                label={resolveTranslation("PAGE.SIGN_IN.ACCOUNT_CONFIRMATION")}
+                            />
+                        </Wrapper>
+                        <PrimaryButton type="submit" isWide>{resolveTranslation("PAGE.SING_UP.TOP_TITLE")}</PrimaryButton>
+                    </form>
+                )}
+            </Formik>
         </>
     );
 };
