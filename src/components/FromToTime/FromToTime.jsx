b3c2e72e39ccd071be071450c785bdc38a902b2c
Vlad Serhiychuk <SVladikO@mail.ru>
2023-04-26 13:54:16 +0300

    [PMA-285] Added possibility to change company schedule time.

--- src/components/FromToTime/FromToTime.jsx
+++ src/components/FromToTime/FromToTime.jsx
@@ -1,25 +1,45 @@
+import {memo, useCallback, useState} from 'react';
+
 import {Wrapper, GroupWrapper, Label} from "./FromToTime.style";
 
 import {Checkbox, Input} from "../index";
 
-function FromToTime({id, weekDay, from="00:00", to="00:00"}) {
-    console.log(id)
+function FromToTime({day, updateWeekScheduleKipper}) {
+    const {isChecked, name, from = "", to = ""} = day;
+
+    const [fromTime, setFromTime] = useState(from);
+    const [toTime, setToTime] = useState(to);
+    const [checked, setChecked] = useState(isChecked);
+
+    const checkboxChangeHandler = useCallback(() => {
+        setChecked(!checked);
+        updateWeekScheduleKipper(name, 'isChecked', !checked);
+    }, [checked])
+    const changeHandlerFromTime = useCallback(value => {
+        setFromTime(value);
+        updateWeekScheduleKipper(name, 'from', value);
+    }, [fromTime])
+    const changeHandlerToTime = useCallback(value => {
+        setToTime(value)
+        updateWeekScheduleKipper(name, 'to', value);
+    }, [toTime])
+    console.log(222)
     return (
         <Wrapper>
             <GroupWrapper>
-                <Checkbox id={id}/>
-                <Label htmlFor={id}>{weekDay}</Label>
+                <Checkbox isChecked={checked} changeHandler={checkboxChangeHandler}/>
+                <Label onClick={checkboxChangeHandler}>{name}</Label>
             </GroupWrapper>
             <GroupWrapper>
                 <span>From</span>
-                <Input value={from}/>
+                <Input value={fromTime} changeHandler={changeHandlerFromTime}/>
             </GroupWrapper>
             <GroupWrapper>
                 <span>To</span>
-                <Input value={to}/>
+                <Input value={toTime} changeHandler={changeHandlerToTime}/>
             </GroupWrapper>
         </Wrapper>
     );
 }
 
-export default FromToTime;
+export default memo(FromToTime);
