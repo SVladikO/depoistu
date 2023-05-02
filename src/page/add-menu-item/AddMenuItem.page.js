8de7b0c155d32c694f50217981c046315cf851ac
Chernukha21 <Chorniy2008@ukr.net>
2023-05-02 18:32:42 +0300

    [PMA-300]-Validated AddMenuItem page

--- src/page/add-menu-item/AddMenuItem.page.js
+++ src/page/add-menu-item/AddMenuItem.page.js
@@ -1,5 +1,6 @@
 import React, {useCallback, useState} from "react";
-
+import {Formik} from "formik";
+import * as Yup from 'yup';
 import {MenuItemPhoto, ImagePlace} from './AddMenuItem.style';
 
 import {
@@ -10,6 +11,9 @@ import {
     SecondaryButton,
     Textarea
 } from '../../components/index'
+import {menu_item_validation} from "../../utils/validation";
+const AddMenuItemSchema = Yup.object().shape(menu_item_validation);
+
 
 const AddMenuItemPage = () => {
     const [name, setName] = useState('');
@@ -36,53 +40,81 @@ const AddMenuItemPage = () => {
 
     return (
         <>
-            <RowSplitter height={'15px'}/>
-            <ContentContainer>
-                <MenuItemPhoto>
-                    {imageURL
-                        ? <img src={imageURL} alt='Food'/>   // setImageURL
-                        : <ImagePlace/>}
-                    <SecondaryButton>{imageURL ? 'Change image' : 'Add image'}</SecondaryButton>
-                </MenuItemPhoto>
-                <Label>Name</Label>
-                <Input
-                    value={name}
-                    changeHandler={nameChangeHandler}
-                    clearHandler={nameClearHandler}
-                    withCleaner
-                />
-                <Label>Price</Label>
-                <Input
-                    value={price}
-                    changeHandler={priceChangeHandler}
-                    clearHandler={priceClearHandler}
-                    withCleaner
-                />
-                <Label>Description</Label>
-                <Textarea
-                    value={description}
-                    changeHandler={descriptionChangeHandler}
-                    clearHandler={descriptionClearHandler}
-                    withCleaner
-                />
-                <Label>Cooking time (in minutes)</Label>
-                <Input
-                    value={cookingTime}
-                    type={'number'}
-                    changeHandler={cookingTimeChangeHandler}
-                    clearHandler={cookingTimeClearHandler}
-                    withCleaner
-                />
-                <Label>Meal Size</Label>
-                <Input
-                    value={size}
-                    type={'number'}
-                    changeHandler={sizeChangeHandler}
-                    clearHandler={sizeClearHandler}
-                    withCleaner
-                />
-            </ContentContainer>
-            <PrimaryButton>Add</PrimaryButton>
+            <Formik
+                initialValues={{
+                    name: '',
+                    price: '',
+                    description: '',
+                    cookingTime: 0,
+                    size: 0
+                }}
+                validationSchema={AddMenuItemSchema}
+                onSubmit={values => {
+                    console.log(values);
+                }}
+            >
+                {({values, setFieldValue, handleSubmit, handleChange, errors}) => (
+                    <form onSubmit={handleSubmit}>
+                        <RowSplitter height={'15px'}/>
+                        <ContentContainer>
+                            <MenuItemPhoto>
+                                {imageURL
+                                    ? <img src={imageURL} alt='Food'/>   // setImageURL
+                                    : <ImagePlace/>}
+                                <SecondaryButton type="button">{imageURL ? 'Change image' : 'Add image'}</SecondaryButton>
+                            </MenuItemPhoto>
+                            <Label>Name</Label>
+                            <Input
+                                value={values.name}
+                                name="name"
+                                changeHandler={handleChange}
+                                clearHandler={() => setFieldValue('name', '')}
+                                withCleaner
+                                errorMessage={errors.name}
+                            />
+                            <Label>Price</Label>
+                            <Input
+                                value={values.price}
+                                name="price"
+                                changeHandler={handleChange}
+                                clearHandler={() => setFieldValue('price', '')}
+                                withCleaner
+                                errorMessage={errors.price}
+                            />
+                            <Label>Description</Label>
+                            <Textarea
+                                value={values.description}
+                                changeHandler={handleChange}
+                                clearHandler={() => setFieldValue('description', '')}
+                                withCleaner
+                                name="description"
+                                errorMessage={errors.description}
+                            />
+                            <Label>Cooking time (in minutes)</Label>
+                            <Input
+                                value={values.cookingTime}
+                                type="number"
+                                changeHandler={handleChange}
+                                clearHandler={() => setFieldValue('cookingTime', '')}
+                                withCleaner
+                                name="cookingTime"
+                                errorMessage={errors.cookingTime}
+                            />
+                            <Label>Meal Size</Label>
+                            <Input
+                                value={values.size}
+                                type="number"
+                                changeHandler={handleChange}
+                                clearHandler={() => setFieldValue('size','')}
+                                withCleaner
+                                name="size"
+                                errorMessage={errors.size}
+                            />
+                        </ContentContainer>
+                        <PrimaryButton isWide type="submit">Add</PrimaryButton>
+                    </form>
+                    )}
+            </Formik>
         </>
     );
 }
