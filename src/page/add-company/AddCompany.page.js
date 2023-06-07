d0f0b483c1f1e6bcb57d3c08e5b037e95466143f
Chernukha21 <Chorniy2008@ukr.net>
2023-06-07 13:40:45 +0300

    [PMA-352]-Added detailed success text to addcompanypage

--- src/page/add-company/AddCompany.page.js
+++ src/page/add-company/AddCompany.page.js
@@ -1,6 +1,6 @@
 import "swiper/css";
 import "swiper/css/pagination";
-import {Link} from "react-router-dom";
+import {Link, useParams} from "react-router-dom";
 import React, {useState, useEffect} from "react";
 
 import {Notification} from "../../components";
@@ -13,13 +13,16 @@ import {BE_API} from '../../utils/fetch'
 import {fetchData} from "../../utils/fetch";
 import {getScheduleAsString} from "../../utils/company";
 import {LOCAL_STORAGE_KEY, LocalStorage} from "../../utils/localStorage";
+import {WarningText, Reference} from "./AddCompany.style";
+import {ReactComponent as LinkArrowIcon} from "../../icons/right-anchor.svg";
+
 
 const AddCompany = () => {
     const CUSTOMER = LocalStorage.get(LOCAL_STORAGE_KEY.CUSTOMER)
     const [isLoading, setIsLoading] = useState(false);
     const [isCompanySaved, setIsCompanySaved] = useState(false);
     const [requestError, setRequestError] = useState("");
-
+    const [newCompanyId, setNewCompanyId] = useState();
     useEffect(() => {
         window.scrollTo(0, 0)
     }, [])
@@ -35,8 +38,9 @@ const AddCompany = () => {
         setIsLoading(true);
 
         fetchData(BE_API.COMPANY.POST_CREATE(), reqObj)
-            .then(() => {
+            .then((res) => {
                 setIsCompanySaved(true);
+                setNewCompanyId(res.body.successMessage.insertId);
                 LocalStorage.remove(LOCAL_STORAGE_KEY.CUSTOMER_COMPANIES)
             })
             .catch(res => setRequestError(res.status + " Error: " + res.body.errorMessage))
@@ -49,15 +53,20 @@ const AddCompany = () => {
 
     if (isCompanySaved) {
         return (
-            <Notification.Success message={"Company created."}>
-                <Link to={URL.CUSTOMER_COMPANIES}>Open my companies</Link>
+            <Notification.Success message={"Company was created"}>
+                <WarningText>
+                    Without menu this company won’t be shown in search
+                </WarningText>
+                <Reference>
+                    <Link to={`${URL.EDIT_MENU}/${newCompanyId}`}>Add menu for this company<LinkArrowIcon/></Link>
+                </Reference>
             </Notification.Success>
         );
     }
-
     return (
         <>
             {requestError && <Notification.Error message={requestError}/>}
+            {isCompanySaved}
             <CompanyView
                 initialValues={initialValues}
                 onSubmit={onSubmit}
