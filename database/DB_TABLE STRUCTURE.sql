c7f29ccbe4f9e7192b8d50f1c017488029e8cfc4
IrinaSerhiichuk <117077135+IrinaSerhiichuk@users.noreply.github.com>
2023-06-20 17:27:38 +0300

    [PMA-394] DB updated DEVELOP_DATA table

--- database/DB_TABLE STRUCTURE.sql
+++ database/DB_TABLE STRUCTURE.sql
@@ -1,41 +1,40 @@
 
 CREATE TABLE MENU_ITEM
 (
-    ID           SERIAL NOT NULL,
-    CATEGORY_ID  INT    NOT NULL,
-    COMPANY_ID   INT    NOT NULL,
-    NAME         TEXT   NOT NULL,
-    DESCRIPTION  TEXT,
-    COOKING_TIME TEXT   NOT NULL,
-    PRICE        INT    NOT NULL,
-    SIZE         TEXT   NOT NULL,
-    IMAGE_URL    TEXT   NOT NULL
+    ID           SERIAL     NOT NULL,
+    CATEGORY_ID  INT        NOT NULL,
+    COMPANY_ID   INT        NOT NULL,
+    NAME         CHAR(30)   NOT NULL,
+    DESCRIPTION  CHAR(200),
+    COOKING_TIME CHAR(2)    NOT NULL,
+    PRICE        CHAR(7)       NOT NULL,
+    SIZE         CHAR(4)    NOT NULL,
+    IMAGE_URL    CHAR(150)  NOT NULL
 );
 
 CREATE TABLE CUSTOMER
 (
-    ID                SERIAL NOT NULL,
-    NAME              TEXT   NOT NULL,
-    PHONE             TEXT    NOT NULL,
-    PASSWORD          TEXT   NOT NULL,
-    EMAIL             TEXT,
+    ID                SERIAL    NOT NULL,
+    NAME              CHAR(30)  NOT NULL,
+    PHONE             CHAR(12)  NOT NULL,
+    PASSWORD          CHAR(12)  NOT NULL,
+    EMAIL             CHAR(30),
     IS_VERIFIED_PHONE BOOLEAN,
     IS_VERIFIED_EMAIL BOOLEAN,
-    JOIN_DATE         TEXT,
-    LIKED_MENU_ITEMS  TEXT
+    JOIN_DATE         CHAR(14)
 );
 
 CREATE TABLE COMPANY
 (
     ID          SERIAL   NOT NULL,
     CUSTOMER_ID INT      NOT NULL,
-    NAME        TEXT     NOT NULL,
-    PHONE       TEXT     NOT NULL,
-    CITY        TEXT     NOT NULL,
-    STREET      TEXT     NOT NULL,
-    JOIN_DATE   TEXT,
-    SCHEDULE    TEXT    NOT NULL,
-    PHOTOS      TEXT
+    NAME        CHAR     NOT NULL,
+    PHONE       CHAR(12)     NOT NULL,
+    CITY        CHAR(5)     NOT NULL,
+    STREET      CHAR(30)     NOT NULL,
+    JOIN_DATE   CHAR(14),
+    SCHEDULE    CHAR(100)    NOT NULL,
+    PHOTOS      VARCHAR(5000)
 );
 
 CREATE TABLE HISTORY
@@ -43,8 +42,8 @@ CREATE TABLE HISTORY
     ID            SERIAL NOT NULL,
     CUSTOMER_ID   INT    NOT NULL,
     COMPANY_ID    INT    NOT NULL,
-    ORDER_DETAILS TEXT,
-    DATE_TIME     TEXT,
+    ORDER_DETAILS CHAR(),
+    DATE_TIME     CHAR(),
     IS_PAID       BOOLEAN,
     IS_PREPARED   BOOLEAN
 );
