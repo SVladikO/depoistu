e39ddfbb9b4504c27bcd88d2bbd198ba8aae69dc
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-08-03 15:24:06 +0300

    Added new route from BE

--- src/page/search-details/SearchDetails.page.js
+++ src/page/search-details/SearchDetails.page.js
@@ -30,7 +30,7 @@ const SearchDetailsPage = () => {
     }, [companyId])
 
     useEffect(() => {
-        fetch(BE_API.MENU_ITEM.GET_BY_COMPANY_ID(companyId))
+        fetch(BE_API.MENU_ITEM.GET_ONLY_VISIBLE_BY_COMPANY_ID(companyId))
             .then(res => res.json())
             .then(data => data.sort((a, b) => a.CATEGORY_ID - b.CATEGORY_ID))
             .then(result => {
@@ -41,25 +41,23 @@ const SearchDetailsPage = () => {
     const menuItemComponents = useMemo(() => {
         const ids = [];
         return menuItems?.map(el => {
-            const {CATEGORY_ID, IS_VISIBLE} = el;
+            const {CATEGORY_ID} = el;
 
-            if (IS_VISIBLE) {
-                if (ids.includes(CATEGORY_ID)) {
-                    return <MenuItem key={el.ID} item={el} />
-                }
-
-                ids.push(CATEGORY_ID);
-
-                return [
-                    <Divider
-                        id={CATEGORY_ID}
-                        key={CATEGORY_MAPPER[CATEGORY_ID].title}
-                    >
-                        {CATEGORY_MAPPER[CATEGORY_ID].title.toUpperCase()}
-                    </Divider>,
-                    <MenuItem key={el.ID} item={el} />
-                ];
+            if (ids.includes(CATEGORY_ID)) {
+                return <MenuItem key={el.ID} item={el}/>
             }
+
+            ids.push(CATEGORY_ID);
+
+            return [
+                <Divider
+                    id={CATEGORY_ID}
+                    key={CATEGORY_MAPPER[CATEGORY_ID].title}
+                >
+                    {CATEGORY_MAPPER[CATEGORY_ID].title.toUpperCase()}
+                </Divider>,
+                <MenuItem key={el.ID} item={el}/>
+            ];
         })?.flat()
     }, [menuItems]);
 
--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -71,6 +71,7 @@ export const BE_API = {
     },
     MENU_ITEM: {
         GET_BY_COMPANY_ID: company_id => `${BE_DOMAIN}/menu/${company_id}`,
+        GET_ONLY_VISIBLE_BY_COMPANY_ID: company_id => `${BE_DOMAIN}/menu/only-visible/${company_id}`,
         POST_CREATE: () => `${BE_DOMAIN}/menu`,
         PUT_UPDATE: () => `${BE_DOMAIN}/menu`,
         DELETE: () => `${BE_DOMAIN}/menu`,
