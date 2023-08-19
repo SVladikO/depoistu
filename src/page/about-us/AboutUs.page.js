1375bac041e53c3da7164641f2e01ea70f893377
Chernukha21 <Chorniy2008@ukr.net>
2023-08-19 15:32:11 +0300

    [DEP-526]-fe-add-about-us-page

--- src/page/about-us/AboutUs.page.js
+++ src/page/about-us/AboutUs.page.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Wrapper,AboutUsItem, Title, Description} from "./AboutUs.page.style";
+import {translate, TRANSLATION as TR} from "../../utils/translation";
+
 
 const AboutUsPage = () => {
     return (
-        <div>
-            We are the best.
-        </div>
+         <Wrapper>
+            <AboutUsItem>
+                <Title>{translate(TR.PAGE.ABOUT_US.WHO_WE_ARE.TOP_TITLE)}</Title>
+                <Description>{translate(TR.PAGE.ABOUT_US.WHO_WE_ARE.DESCRIPTION)}</Description>
+                <Description>{translate(TR.PAGE.ABOUT_US.WHO_WE_ARE.PROBLEM)}</Description>
+            </AboutUsItem>
+             <AboutUsItem>
+                 <Title>{translate(TR.PAGE.ABOUT_US.WHAT_UNITED_US.TOP_TITLE)}</Title>
+                 <Description>{translate(TR.PAGE.ABOUT_US.WHAT_UNITED_US.DESCRIPTION)}</Description>
+             </AboutUsItem>
+             <AboutUsItem>
+                 <Title>{translate(TR.PAGE.ABOUT_US.WHEN_DID_WE_START.TOP_TITLE)}</Title>
+                 <Description>{translate(TR.PAGE.ABOUT_US.WHEN_DID_WE_START.DESCRIPTION)}</Description>
+             </AboutUsItem>
+             <AboutUsItem>
+                 <Title>{translate(TR.PAGE.ABOUT_US.WHEN_WAS_THE_FIRST_VERSION_RELEASED.TOP_TITLE)}</Title>
+                 <Description>{translate(TR.PAGE.ABOUT_US.WHEN_WAS_THE_FIRST_VERSION_RELEASED.DESCRIPTION)}</Description>
+             </AboutUsItem>
+             <AboutUsItem>
+                 <Title>{translate(TR.PAGE.ABOUT_US.MAIN_IDEA_OF_THE_PROJECT.TOP_TITLE)}</Title>
+                 <Description>{translate(TR.PAGE.ABOUT_US.MAIN_IDEA_OF_THE_PROJECT.DESCRIPTION)}</Description>
+                 <Description>{translate(TR.PAGE.ABOUT_US.MAIN_IDEA_OF_THE_PROJECT.PROBLEM)}</Description>
+             </AboutUsItem>
+             <AboutUsItem>
+                 <Title>{translate(TR.PAGE.ABOUT_US.COST.TOP_TITLE)}</Title>
+                 <Description>{translate(TR.PAGE.ABOUT_US.COST.DESCRIPTION)}</Description>
+             </AboutUsItem>
+         </Wrapper>
     );
 };
 
---
+++ src/page/about-us/AboutUs.page.style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+import {COLOR, SHADOW, BORDER_RADIUS} from "../../utils/theme";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  padding: 10px;
+  ${SHADOW};
+  color: ${COLOR.ACCENT1};
+  background: ${COLOR.ACCENT4};
+  border-radius: ${p => p.borderRadius ? p.borderRadius : BORDER_RADIUS.SECOND};
+`;
+export const AboutUsItem = styled.div`
+  margin: 0 0 20px 0;
+`;
+export const Title = styled.div`
+  font-weight: 900;
+  font-size: 22px;
+  line-height: 26.4px;
+  margin: 0 0 10px 0;
+`;
+export const Description = styled.div`
+  font-weight: 500;
+  font-size: 22px;
+  line-height: 26.4px;
+  &:last-child {
+    margin: 5px 0 0 0;
+  }
+`;
