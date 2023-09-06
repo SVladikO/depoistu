27c26dc52b539d812e06692ce85b7fba184ec5e4
Pavlo Serhiichuk <61013347+pavlo-serhiichuk@users.noreply.github.com>
2023-09-06 17:51:44 +0300

    [Dep-564] Added checking of company existence. Search details page

--- src/page/search-details/SearchDetails.style.js
+++ src/page/search-details/SearchDetails.style.js
@@ -4,12 +4,3 @@ import {COLOR} from "../../utils/theme";
 export const Wrapper = styled.div`
   background: transparent;
 `;
-
-export const CategoryTitle = styled.div`
-  font-size: 20px;
-  line-height: 24px;
-  color: ${COLOR.ACCENT4};
-  padding: 16px 0 12px 10px;
-  background: rgb(0 0 0 / 70%);
-  //margin: 8px 0 2px;
-`;
