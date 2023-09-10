847a5861b9a639189fcebceb637bbc4bd1abb2b9
Chernukha21 <Chorniy2008@ukr.net>
2023-09-10 17:10:24 +0300

    [DEP-567]-Made notifications disappear after 3 seconds

--- src/page-view/notification/NotificationView.style.js
+++ src/page-view/notification/NotificationView.style.js
@@ -5,15 +5,22 @@ import {hexToRgbA} from "utils/theme";
 export const Wrapper = styled.div`
   padding: 15px 6px 0;
   position: absolute;
-  top: 0;
+  top: -15px;
   width: 100%;
+  opacity: 0;
   background: ${hexToRgbA('#000', 0.5)};
   z-index: 8;
-  animation: myfirst 2s;
+  animation: myfirst 4s;
   animation-direction: alternate;
 
   @keyframes myfirst {
-    0%   {top: -300px;}
-    100% {top: 0;}
+    0% {
+        top: -300px;
+        opacity: 0
+    }
+    100% {
+        opacity: 1;
+        top: 0;
+    }
   }
 `;
