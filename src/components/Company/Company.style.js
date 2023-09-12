7b84c6194b0d848323e7e76c8aa28a1cd5184bd0
Chernukha21 <Chorniy2008@ukr.net>
2023-09-12 15:35:07 +0300

    [DEP-580]-Fixed margin in company

--- src/components/Company/Company.style.js
+++ src/components/Company/Company.style.js
@@ -9,7 +9,8 @@ export const Wrapper = styled.div`
   perspective: 1px;
   background-color: ${COLOR.ACCENT4};
   margin: ${p => p.withMoreInfo ? '0 0 2px 0' : '0 0 10px 0'};
-  .swiper-pagination-bullet{
+
+  .swiper-pagination-bullet {
     width: 10px;
     height: 10px;
     background-color: ${COLOR.ACCENT4};
@@ -43,7 +44,7 @@ export const CompanyInfo = styled.div`
   }
 
   & > *:last-child {
-    margin: 0;
+    margin: 0 0 20px 0;
   }
 `;
 
@@ -60,7 +61,7 @@ export const OpenStatus = styled.span`
   color: ${COLOR.ACCENT1};
   display: inline-flex;
   margin: 0 7px 0 0;
-  
+
   svg {
     display: inline-block;
     width: 15px;
@@ -75,6 +76,7 @@ export const CloseStatus = styled.span`
   font-size: 20px;
   line-height: 24px;
   display: inline-flex;
+
   svg {
     display: inline-block;
     width: 15px;
@@ -91,11 +93,11 @@ export const Closes = styled.span`
   color: ${COLOR.ACCENT1};
   position: relative;
   display: inline-block;
- 
+
   & > span {
     display: inline-block;
   }
-  
+
 `;
 export const Name = styled.div`
   font-weight: 700;
@@ -114,9 +116,10 @@ export const LocationWrapper = styled.div`
   font-size: 20px;
   border-radius: ${BORDER_RADIUS.FOURTH};
   border: ${props => props.withAdditionalStyles ? `1px solid ${COLOR.ACCENT5}` : 'none'};
-  //padding: ${props => props.withAdditionalStyles ? '2px 10px 4px 10px' : '0'};
+    //padding: ${props => props.withAdditionalStyles ? '2px 10px 4px 10px' : '0'};
   margin: 0 0 10px 0;
   color: ${COLOR.ACCENT1};
+
   svg {
     width: 10px;
     height: 16px;
@@ -135,5 +138,5 @@ export const Address = styled.div`
   font-size: 20px;
   line-height: 24px;
   color: ${COLOR.ACCENT5};
- 
+
 `;
