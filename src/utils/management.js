d84bdb977851c3856efbfe9f7df100e7fcf4bf1a
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-09-28 17:31:21 +0300

    [DEP-592] Changed placement of project details data from console to components page

--- src/utils/management.js
+++ src/utils/management.js
@@ -1,14 +1,10 @@
 import packageInfo from "../../package.json";
 import {checkAccess} from "./security";
-import {showDevelopmentPageUrls} from "./log";
 
 
 const devManagement = () => {
     console.log(`v${packageInfo.version}`);
     checkAccess();
-    showDevelopmentPageUrls();
-
-    // localStorage.clear()
 }
 
 export default devManagement;
