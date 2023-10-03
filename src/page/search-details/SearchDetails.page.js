5fb0c66a8f3892596c15e295904eeb7d808c6312
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-10-03 23:15:08 +0300

    [DEP-612] Added pages menu and favorite and navigation on them

--- src/page/search-details/SearchDetails.page.js
+++ src/page/search-details/SearchDetails.page.js
@@ -19,14 +19,24 @@ const SearchDetailsPage = () => {
     useScrollUp();
     const navigate = useNavigate();
     let companyId = +useParams().companyId;
+
     const [isLoadingMenu, setIsLoadingMenu] = useState(false);
     const [isLoadingCompany, setIsLoadingCompany] = useState(false);
+
     const [company, setCompany] = useLocalStorage(LOCAL_STORAGE_KEY.SEARCH_DETAILS_COMPANY)
     const [menuItems, setMenuItems] = useLocalStorage(LOCAL_STORAGE_KEY.SEARCH_DETAILS_MENU);
 
     useEffect(() => {
-        if (company) {
+        LocalStorage.set(LOCAL_STORAGE_KEY.SEARCH_DETAILS_SELECTED_COMPANY_ID, companyId)
+    })
+
+    useEffect(() => {
+        if (!companyId) {
             return
+        }
+
+        if (company && company.id === companyId) {
+            return;
         }
 
         setIsLoadingCompany(true)
@@ -45,9 +55,14 @@ const SearchDetailsPage = () => {
     }, [companyId])
 
     useEffect(() => {
-        if (menuItems) {
+        if (!companyId) {
             return
         }
+
+        if (menuItems && company.id === companyId) {
+            return
+        }
+
         setIsLoadingMenu(true);
         const menuLoadingDelay = stopLoadingWithDelay([() => setIsLoadingMenu(false)]);
 
@@ -85,13 +100,38 @@ const SearchDetailsPage = () => {
             </NotificationTDB>
         )
     }
+    //         <NotificationTDB title={translate(TR.PAGE.COMPANY_DETAILS.COMPANY_DOESNT_EXIST)}>
+    //             <PrimaryButton isWide clickHandler={() => {
+    //
+    //                 // We delete these data for case when
+    //                 // company was deleted between
+    {/*                // customer found list of companies per city*/
+    }
+    {/*                // and*/
+    }
+    {/*                // open company*/
+    }
+    {/*                LocalStorage.remove(LOCAL_STORAGE_KEY.COMPANY_SEARCH_SELECTED_CITY_ID)*/
+    }
+    //                 LocalStorage.remove(LOCAL_STORAGE_KEY.COMPANY_SEARCH_SELECTED_REGION_ID)
+    //                 LocalStorage.remove(LOCAL_STORAGE_KEY.COMPANY_SEARCH_RESULT)
+    //
+    //                 navigate(ROUTER.SEARCH.URL)
+    //             }}>
+    //                 {translate(TR.GO_TO_A_SEARCH_PAGE)}
+    //             </PrimaryButton>
+    //         </NotificationTDB>
+    //     )
+    // }
 
     return (
         <Wrapper>
-            {isLoadingCompany && <NotificationLoading>{translate(TRANSLATION.NOTIFICATION.COMPANY.LOADING_COMPANY)}</NotificationLoading>}
+            {isLoadingCompany &&
+                <NotificationLoading>{translate(TRANSLATION.NOTIFICATION.COMPANY.LOADING_COMPANY)}</NotificationLoading>}
             {!isLoadingCompany && company && <Company company={company} withMoreInfo/>}
 
-            {isLoadingMenu && <NotificationLoading>{translate(TRANSLATION.NOTIFICATION.LOADING_MENU)}</NotificationLoading>}
+            {isLoadingMenu &&
+                <NotificationLoading>{translate(TRANSLATION.NOTIFICATION.LOADING_MENU)}</NotificationLoading>}
 
             {!isLoadingMenu && !!menuItems?.length && (
                 <>
