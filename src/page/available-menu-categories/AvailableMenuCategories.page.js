738f63f69bd96eb172400838c2a669ca5153363a
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-10-06 10:39:47 +0300

    [DEP-616] Added categories page for business

--- src/page/available-menu-categories/AvailableMenuCategories.page.js
+++ src/page/available-menu-categories/AvailableMenuCategories.page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Wrapper, Title, Description} from "./AvailableMenuCategories.page.style";
+import {Wrapper, Table} from "./AvailableMenuCategories.page.style";
 
 import {useScrollUp} from "utils/hook";
 import {translate, TRANSLATION} from "utils/translation";
@@ -8,47 +8,40 @@ import {CATEGORY_BAR, CATEGORY_DESSERTS, CATEGORY_HOT_DRINKS, CATEGORY_KITCHEN} 
 const AvailableMenuCategoriesPage = () => {
     useScrollUp();
 
-    const renderCategoryTranslations = () => {
-        const renderGroup = groupTitle =>
-            <tr>
-                <td style={{padding: "20px 0 10px"}}>
-                    <h2 style={{textAlign: 'center'}}>{groupTitle}</h2>
-                </td>
-            </tr>
+    const renderGroup = groupTitle =>
+        <tr>
+            <td colSpan={2} style={{padding: "10px 0"}}>
+                <h2 style={{textAlign: 'center'}}>{groupTitle}</h2>
+            </td>
+        </tr>
 
-        const render = categories =>
-            categories.map(
-                category => (
-                    <tr>
-                        <td style={{padding: '4px'}}>{translate(category.title)}</td>
-                        <td style={{padding: '4px'}}>{translate(category.measurement)}</td>
-                    </tr>
-                ))
+    const render = categories =>
+        categories.map(
+            (category, index) => (
+                <tr key={category.title + index}>
+                    <td style={{padding: '4px'}}>{translate(category.title)}</td>
+                    <td style={{padding: '4px'}}>{translate(category.measurement)}</td>
+                </tr>
+            ))
 
-
-        return (
-            <div>
-                <table style={{margin: '0 auto'}}>
-                    <tbody>
-                    {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.KITCHEN))}
-                    {render(CATEGORY_KITCHEN)}
-                    {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.DESSERTS))}
-                    {render(CATEGORY_DESSERTS)}
-                    {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.HOT_DRINKS))}
-                    {render(CATEGORY_HOT_DRINKS)}
-                    {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.BAR))}
-                    {render(CATEGORY_BAR)}
-                    </tbody>
-                </table>
-            </div>
-        )
-    }
 
     return (
-        <Wrapper>
-            {renderCategoryTranslations()}
-        </Wrapper>
+        <>
+            <Table>
+                <tbody>
+                {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.KITCHEN))}
+                {render(CATEGORY_KITCHEN)}
+                {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.DESSERTS))}
+                {render(CATEGORY_DESSERTS)}
+                {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.HOT_DRINKS))}
+                {render(CATEGORY_HOT_DRINKS)}
+                {renderGroup(translate(TRANSLATION.TOP_CATEGORIES.BAR))}
+                {render(CATEGORY_BAR)}
+                </tbody>
+            </Table>
+        </>
     )
+
 };
 
 export default AvailableMenuCategoriesPage;
--- src/page/available-menu-categories/AvailableMenuCategories.page.style.js
+++ src/page/available-menu-categories/AvailableMenuCategories.page.style.js
@@ -1,13 +1,9 @@
 import styled from 'styled-components';
-import {COLOR, SHADOW, FONT, BORDER_RADIUS} from "utils/theme";
 
 export const Wrapper = styled.div`
   width: 100%;
-  padding: 16px;
 `;
 
-export const Title = styled.div`
-`;
-export const Description = styled.div`
-  ${FONT.SIZE_20};
+export const Table = styled.table`
+  margin: 0 auto;
 `;
