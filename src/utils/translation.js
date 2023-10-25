7860eef1b6395e58eb5608f30bfaebf8ab908e97
svladiko <vlad.serhiychuk@gmail.com>
2023-10-25 16:55:47 +0300

    [DEP-630] Added english translation

--- src/utils/translation.js
+++ src/utils/translation.js
@@ -595,58 +595,58 @@ export const TRANSLATION = {
             CONTENT: {
                 en: [
                     {
-                        question: "Яка головна ідея проекту ?",
+                        question: "What is the main idea of the project?",
                         answers: [
-                            "Всі заклади харчування України на одному сайті depoistu.com",
-                            "Якщо вашого закладу немає на нашому сайті, значить вас взагалі не існує."
+                            "All catering establishments of Ukraine on one website depoistu.com",
+                            "If your catering establishments is not on our website, then you do not exist at all."
                         ]
                     },
                     {
-                        question: "Яка вартість ?",
+                        question: "What is the cost?",
                         answers: [
-                            "Перші чотири місяці після запуску проекту БЕЗКОШТОВНО.",
+                            "The first four months after the launch of the project are FREE.",
                             "З 1.11.2023 - 1.04.2024",
                         ]
                     },
                     {
-                        question: "Чому ви потрібні бізнесу ?",
+                        question: "Why do businesses need you?",
                         withCounter: true,
                         answers: [
-                            "З нами ви економите час, гроші, сили, нерви на планування, створення, підтримку, просування власного сайту. Круто, погодьтесь. ",
-                            "Можливість змінювати меню в будь-яку пору доби.",
-                            "Можливість бути сучасними.",
-                            "Ваші клієнти тепер переглядають меню на будь-якій відстані в будь-яку пору доби.",
-                            "Про вас більше знають."
+                            "With us, you save time, money, energy, and nerves for planning, creating, maintaining, and promoting your own website. Cool, don't you agree?",
+                            "The possibility of changing the menu at any time of the day.",
+                            "The opportunity to be modern.",
+                            "Your customers can now view the menu from any distance at any time of the day.",
+                            "More people know about you."
                         ]
                     },
                     {
-                        question: "Чому ви потрібні клієнтам ?",
+                        question: "Why do customers need you?",
                         withCounter: true,
                         answers: [
-                            "Швидше знаходите заклад харчування в любому куточку України. В новому місці.",
-                            "Швидше знаходите позицію меню.",
-                            "Швидше замовляєте.",
-                            "Швидше отримуєте бажане.",
+                            "Find a restaurant in any corner of Ukraine faster. In a new place.",
+                            "Find the menu position faster.",
+                            "Order faster.",
+                            "You get what you want faster.",
                         ]
                     },
                     {
-                        question: "Як бізнесу заробляти швидше ?",
+                        question: "How can a business make money faster?",
                         answers: [
-                            "Розмістіть QR MENU в своєму закладі на кожному столі.",
-                            "Це ваша інвестиція в загальний успіх.",
-                            "Всі бізнеси познайомлять своїх клієнтів з цим сайтом.",
-                            "І от ми отримали екосистему.",
-                            "Клієнт - Бізнес."
+                            "Place a QR MENU in your establishment on every table.",
+                            "This is your investment in overall success.",
+                            "All businesses will introduce their customers to this site.",
+                            "And so we got an ecosystem.",
+                            "Client - Business."
                         ]
                     },
                     {
-                        question: "Коли випустили першу версію?",
-                        answers: ["1 Листопада 2023 року"]
+                        question: "When was the first version released?",
+                        answers: ["November 1, 2023"]
                     },
                     {
-                        question: "Що вас об`єднує?",
+                        question: "What unites you?",
                         answers: [
-                            "Бажаємо вирішити спільну проблему: 'Швидко знаходити заклади харчування в Україні.' ",
+                            "We want to solve a common problem: 'Quickly find food establishments in Ukraine.' ",
                         ]
                     },
                 ],
@@ -673,7 +673,7 @@ export const TRANSLATION = {
                             "Можливість змінювати меню в будь-яку пору доби.",
                             "Можливість бути сучасними.",
                             "Ваші клієнти тепер переглядають меню на будь-якій відстані в будь-яку пору доби.",
-                            "Про вас більше знають."
+                            "Більше людей знають про вас."
                         ]
                     },
 
