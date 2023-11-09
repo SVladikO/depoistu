014c822affe77390ba20bd028528cfd283f8d813
v.serhiichuk <Vladyslav.Serhiichuk@playtech.com>
2023-11-09 18:48:21 +0200

    Changed company style. Fixed icons size, margin

--- src/components/Company/Company.js
+++ src/components/Company/Company.js
@@ -8,24 +8,13 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 import {ReactComponent as LocationIcon} from "assets/icons/location.svg";
-import {ReactComponent as PhoneIcon} from "assets/icons/phone.svg";
 import {ReactComponent as TimeIcon} from "assets/icons/time.svg";
+import {ReactComponent as PhoneIcon} from "assets/icons/phone.svg";
 import {ReactComponent as Heart1Icon} from "assets/icons/heart1.svg";
 import {ReactComponent as Heart2Icon} from "assets/icons/heart2.svg";
 
-
 import {
-    Wrapper,
-    ImageSection,
-    Name,
-    Content,
-    CompanyInfo,
-    Schedule,
-    OpenStatus,
-    FirstRow,
-    Closes,
-    LocationWrapper,
-    CloseStatus
+    Wrapper, ImageSection, Name, Content, CompanyInfo, Schedule, OpenStatus, FirstRow, Closes, LocationWrapper,
 } from "./Company.style";
 
 import {ThirdButton} from "components/Buttons/ThirdButton";
@@ -35,7 +24,9 @@ import {parseSchedule} from "utils/company";
 import {BE_API, fetchData} from "utils/fetch";
 import {CITY_TRANSLATION_IDS} from "utils/cities";
 import {translate, TRANSLATION as TR, truncate} from "utils/translation";
-import {addToFavoriteCompanies, deleteFromFavoriteCompanies} from "../../features/favorite-company/favoriteComapnySlice";
+import {
+    addToFavoriteCompanies, deleteFromFavoriteCompanies
+} from "../../features/favorite-company/favoriteComapnySlice";
 import {publishNotificationEvent} from "../../utils/event";
 
 const Company = ({company, withMoreInfo, children, clickHandler}) => {
@@ -53,58 +44,59 @@ const Company = ({company, withMoreInfo, children, clickHandler}) => {
 
     const parsedSchedule = parseSchedule(company.schedule);
 
-    const CompanyLocationButton = () => {
+    const renderCityStreet = () => {
         if (withMoreInfo) {
             const address = `,${company.street}, ${translate(CITY_TRANSLATION_IDS[company.cityId])}`
             let addressUrl = `https://www.google.com/maps?q=${encodeURIComponent(address)}`
 
-            return (
-                <Link to={addressUrl} target="_blank" rel="noopener">
-                    <ThirdButton>
-                        <LocationIcon/>
-                        {truncate(`${translate(CITY_TRANSLATION_IDS[company.cityId])}, ${company.street}`, 28)}
-                    </ThirdButton>
-                </Link>
-            )
+            return (<Link to={addressUrl} target="_blank" rel="noopener">
+                <ThirdButton>
+                    <LocationIcon className="location_icon"/>
+                    {truncate(`${translate(CITY_TRANSLATION_IDS[company.cityId])}, ${company.street}`, 40)}
+                </ThirdButton>
+            </Link>)
         }
 
-        return (
-            <LocationWrapper>
-                <LocationIcon/>{translate(CITY_TRANSLATION_IDS[company.cityId])}, {company.street}
-            </LocationWrapper>
-        );
+        return (<LocationWrapper>
+            <LocationIcon/>{translate(CITY_TRANSLATION_IDS[company.cityId])}, {company.street}
+        </LocationWrapper>);
     }
 
-    const Images = () => (
-        <ImageSection>
-            <Swiper
-                modules={[Navigation, Pagination]}
-                slidesPerView={1}
-                navigation
-                pagination={{clickable: true}}
-            >
-                {company.photos && company.photos.split(', ')
-                    .map((src, i) =>
-                        <SwiperSlide key={i}>
-                            <img src={src} alt="#"/>
-                        </SwiperSlide>
-                    )
-                }
-            </Swiper>
-        </ImageSection>
-    );
+    const Images = () => (<ImageSection>
+        <Swiper
+            modules={[Navigation, Pagination]}
+            slidesPerView={1}
+            navigation
+            pagination={{clickable: true}}
+        >
+            {company.photos && company.photos.split(', ')
+                .map((src, i) => <SwiperSlide key={i}>
+                    <img src={src} alt="#"/>
+                </SwiperSlide>)}
+        </Swiper>
+    </ImageSection>);
 
-    const renderDaySchedule = () => (
-        <Schedule>
-            {parsedSchedule.isCompanyOpenNow
-                ? <>
-                    <OpenStatus><TimeIcon/>{translate(TR.COMPONENTS.COMPANY.STATUS_OPEN)}</OpenStatus>
-                    <Closes><span>{translate(TR.COMPONENTS.COMPANY.TILL)} {parsedSchedule.currentDay.to}</span></Closes>
-                </>
-                : <CloseStatus><TimeIcon/>{translate(TR.COMPONENTS.COMPANY.STATUS_CLOSE)}</CloseStatus>
-            }
-        </Schedule>
-    )
+    const renderDaySchedule = () => (<Schedule>
+        <OpenStatus>
+            <TimeIcon className="time_icon"/>
+            {parsedSchedule.isCompanyOpenNow ? translate(TR.COMPONENTS.COMPANY.STATUS_OPEN) : translate(TR.COMPONENTS.COMPANY.STATUS_CLOSE)}
+        </OpenStatus>
+        {parsedSchedule.isCompanyOpenNow &&
+            <Closes>{translate(TR.COMPONENTS.COMPANY.TILL)} {parsedSchedule.currentDay.to}</Closes>}
+    </Schedule>)
+
+    const renderPhone = phone => withMoreInfo && phone && (<a href={`tel:${phone}`}>
+        <ThirdButton>
+            <PhoneIcon className="phone_icon"/>
+            {phone}
+        </ThirdButton>
+    </a>)
+
+    const renderPhones = () => (<>
+        {renderPhone(company.phone1)}
+        {renderPhone(company.phone2)}
+        {renderPhone(company.phone3)}
+    </>)
 
     const likeCompany = e => {
         e.stopPropagation();
@@ -120,37 +112,28 @@ const Company = ({company, withMoreInfo, children, clickHandler}) => {
             .catch(e => publishNotificationEvent.error(e.body.errorMessage))
     }
 
-    return (
-        <Wrapper withMoreInfo={withMoreInfo} onClick={clickHandler}>
-            {/*<Images />*/}
-            <Content>
-                <CompanyInfo>
-                    <FirstRow>
-                        <Name>{company.name}</Name>
-                        {
-                            customer
-                                ? isLikedByCurrentCustomer
-                                    ? <Heart2Icon onClick={unlikeCompany} />
-                                    : <Heart1Icon onClick={likeCompany} />
-                                : null
-                        }
+    return (<Wrapper withMoreInfo={withMoreInfo} onClick={clickHandler}>
+        {/*<Images />*/}
+        <Content>
+            <CompanyInfo>
+                <FirstRow>
+                    <Name>{company.name}</Name>
+                    {customer
+                        ? isLikedByCurrentCustomer
+                            ? <Heart2Icon className="like_company_icon" onClick={unlikeCompany}/>
+                            : <Heart1Icon className="like_company_icon" onClick={likeCompany}/>
+                        : null}
 
-                    </FirstRow>
-                    {renderDaySchedule()}
-                    <CompanyLocationButton/>
-                    {withMoreInfo &&
-                        <a href={`tel:${company.phone1}`}><ThirdButton><PhoneIcon/>{company.phone1}</ThirdButton></a>}
-                    {withMoreInfo && company.phone2 &&
-                        <a href={`tel:${company.phone2}`}><ThirdButton><PhoneIcon/>{company.phone2}</ThirdButton></a>}
-                    {withMoreInfo && company.phone3 &&
-                        <a href={`tel:${company.phone3}`}><ThirdButton><PhoneIcon/>{company.phone3}</ThirdButton></a>}
-                    {withMoreInfo && company.schedule && company.schedule.length &&
-                        <ScheduleDetails scheduleAsArray={parsedSchedule.workDays}/>}
-                </CompanyInfo>
-                {children}
-            </Content>
-        </Wrapper>
-    );
+                </FirstRow>
+                {renderCityStreet()}
+                {renderDaySchedule()}
+                {renderPhones()}
+                {withMoreInfo && company.schedule && company.schedule.length &&
+                    <ScheduleDetails scheduleAsArray={parsedSchedule.workDays}/>}
+            </CompanyInfo>
+            {children}
+        </Content>
+    </Wrapper>);
 };
 
 
