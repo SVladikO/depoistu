05abad7f48ce9442015cb427e4ed3af620dbd6f3
svladiko <vlad.serhiychuk@gmail.com>
2023-12-24 18:12:17 +0200

    Added border to ScrollUpButton

--- src/components/ScrollUpButton/ScrollUpButton.style.js
+++ src/components/ScrollUpButton/ScrollUpButton.style.js
@@ -2,25 +2,26 @@ import styled from 'styled-components'
 import {BORDER_RADIUS, COLOR} from "../../utils/theme";
 
 export const ToTopButtonWrapper = styled.a`
-  position: fixed;
-  bottom: 103px;
-  right: ${p => `calc(100vw / 2 - ${p.rightMargin})`};
-  z-index: 19;
-  transform: rotate(180deg);
-  box-sizing: border-box;
-  width: 50px;
-  height: 50px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  visibility: ${p => p.isVisible ? 'visible' : 'hidden'};
-  border-radius: ${BORDER_RADIUS.CIRCLE};
-  background-color: ${COLOR.ACCENT1};
-  box-shadow: 0px -1px 4px 0px rgba(0,0,0, 0.11);
-  opacity: ${p => p.isVisible ? 1: 0};
-  transition: opacity 0.5s, visibility 0.3s;
-  
-  & > svg {
-    fill: ${COLOR.ACCENT4};
-  }
+    position: fixed;
+    bottom: 103px;
+    right: ${p => `calc(100vw / 2 - ${p.rightMargin})`};
+    z-index: 19;
+    transform: rotate(180deg);
+    box-sizing: border-box;
+    width: 50px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    visibility: ${p => p.isVisible ? 'visible' : 'hidden'};
+    border-radius: ${BORDER_RADIUS.CIRCLE};
+    background-color: ${COLOR.ACCENT1};
+    box-shadow: 0px -1px 4px 0px rgba(0, 0, 0, 0.11);
+    opacity: ${p => p.isVisible ? 1 : 0};
+    transition: opacity 0.5s, visibility 0.3s;
+    border: solid 2px ${COLOR.ACCENT4};
+
+    & > svg {
+        fill: ${COLOR.ACCENT4};
+    }
 `
