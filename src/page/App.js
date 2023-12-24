d519254c06db5ca08b9c0a5fd18837b2941d5dca
svladiko <vlad.serhiychuk@gmail.com>
2023-12-24 14:31:36 +0200

    [DEP-756] FE Restrict access to remote stage and develop

--- src/page/App.js
+++ src/page/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {AllRoutes} from "utils/routes";
 import LanguagePopup from "features/language/LanguagePopup";
 import ShowIntro from "parts/showIntro/ShowIntro";
+import {checkAccessForDevEnv} from "../utils/security";
+
+checkAccessForDevEnv();
 
 const App = () => {
 
