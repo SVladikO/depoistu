24834ac0bbef4b6c80d5457fbceaac89355c21a5
svladiko <vlad.serhiychuk@gmail.com>
2024-01-06 01:01:00 +0200

    Turned back border for menu item

--- src/components/MenuItem/MenuItem.style.js
+++ src/components/MenuItem/MenuItem.style.js
@@ -8,6 +8,7 @@ export const Wrapper = styled.div`
   line-height: 19px;
   background: ${p => p.isItemVisible ? COLOR.ACCENT4 : COLOR.ACCENT7};
   border-radius: ${BORDER_RADIUS.MENU_ITEM};
+  border-bottom: solid 1px ${COLOR.ACCENT9};
   display: flex;
   flex-direction: column;  
 
