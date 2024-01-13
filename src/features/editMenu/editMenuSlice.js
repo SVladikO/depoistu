d5970d2c9b6b56f3f3ed26b2cdda3f810b2b8a1f
svladiko <vlad.serhiychuk@gmail.com>
2024-01-13 06:02:28 +0200

    Added redux part of edit menu.

---
+++ src/features/editMenu/editMenuSlice.js
@@ -0,0 +1,122 @@
+import createSliceCustom from "features/utils";
+import {fetchGetMenuItemsByCompanyId, fetchPostMenuItem, fetchDeleteMenuItem, fetchPutMenuItem} from "./thunks";
+import {errorHandler} from "utils/management";
+
+const initialState = {
+    company_id: undefined,
+    editMenuItems: [],
+    editMenuItemCandidate: undefined,
+    isGetMenuItemsLoading: false,
+    isAddMenuItemLoading: false,
+    isUpdateMenuItemLoading: false,
+    isDeleteMenuItemLoading: false,
+}
+
+export const editMenuSlice = createSliceCustom({
+    name: 'editMenu',
+    initialState,
+    reducers: {
+        initEditMenuItems: (state, action) => {
+            console.log('initEditMenuItems( ', action)
+            state.editMenuItems = action.payload.map(i => ({...i, isImageVisible: false}))
+        },
+        resetAllEditMenu: (state) => {
+            console.log('resetAllEditMenu( ')
+            state.company_id = undefined;
+            state.editMenuItems = [];
+            state.editMenuItemCandidate = undefined;
+            state.isGetMenuItemsLoading = false;
+            state.isAddMenuItemLoading = false;
+            state.isUpdateMenuItemLoading = false;
+            state.isDeleteMenuItemLoading = false;
+        },
+        setCompanyIdToEditMenu: (state, action) => {
+            console.log('setCompanyIdToEditMenu( ')
+            state.company_id = action.payload
+        },
+        addEditMenuItemCandidate: (state, action) => {
+            console.log('addEditMenuItemCandidate(', action.payload.id, action.payload.name);
+            state.editMenuItemCandidate = action.payload
+        },
+        changeIsVisibleEditMenu: (state, action) => {
+            const filtered = state.editMenuItems.find(i => i.id === action.payload);
+            filtered.isVisible = +(!filtered.isVisible);
+        },
+        addMenuItem: (state, action) => {
+            console.log('addMenuItem( ', action)
+            state.editMenuItems.push(action.payload)
+        },
+        updateMenuItem: (state, action) => {
+            const filtered = state.editMenuItems.filter(i => i.id !== action.payload.id);
+            state.editMenuItems = [...filtered, action.payload];
+        },
+        deleteMenuItem: (state, action) => {
+            const filtered = state.editMenuItems.filter(i => i.id !== action.payload.id);
+            state.editMenuItems = filtered;
+        },
+    },
+    extraReducers: {
+        [fetchGetMenuItemsByCompanyId.pending]: (state) => {
+            state.isGetMenuItemsLoading = true
+        },
+        [fetchGetMenuItemsByCompanyId.fulfilled]: (state, action) => {
+            state.isGetMenuItemsLoading = false
+            state.editMenuItems = action.payload;
+        },
+        [fetchGetMenuItemsByCompanyId.rejected]: (state, error) => {
+            state.isGetMenuItemsLoading = false
+            errorHandler(error.payload)
+        },
+
+        [fetchPostMenuItem.pending]: (state) => {
+            state.isAddMenuItemLoading = true
+        },
+        [fetchPostMenuItem.fulfilled]: (state, action) => {
+            state.isAddMenuItemLoading = false
+            console.log('fetchPostMenuItem(', action.payload)
+            state.editMenuItems.push(action.payload)
+        },
+        [fetchPostMenuItem.rejected]: (state, error) => {
+            state.isAddMenuItemLoading = false
+            errorHandler(error.payload)
+        },
+
+        [fetchPutMenuItem.pending]: (state) => {
+            state.isUpdateMenuItemLoading = true
+        },
+        [fetchPutMenuItem.fulfilled]: (state, action) => {
+            state.isUpdateMenuItemLoading = false
+            const filtered = state.editMenuItems.filter(i => i.id !== action.payload.id);
+            state.editMenuItems = [...filtered, action.payload];
+        },
+        [fetchPutMenuItem.rejected]: (state, error) => {
+            state.isUpdateMenuItemLoading = false
+            errorHandler(error.payload)
+        },
+
+        [fetchDeleteMenuItem.pending]: (state) => {
+            state.isCompanyLoading = true
+        },
+        [fetchDeleteMenuItem.fulfilled]: (state, action) => {
+            state.isCompanyLoading = false
+            const filtered = state.editMenuItems.filter(i => i.id !== action.payload);
+            console.log('length:', action, state.editMenuItems.length, filtered.length)
+            state.editMenuItems = filtered;
+        },
+        [fetchDeleteMenuItem.rejected]: (state) => {
+            state.isCompanyLoading = false
+        },
+    }
+});
+
+export const {
+    initEditMenuItems,
+    resetAllEditMenu,
+    setCompanyIdToEditMenu,
+    addEditMenuItemCandidate,
+    changeIsVisibleEditMenu,
+    addMenuItem,
+    updateMenuItem,
+    deleteMenuItem
+} = editMenuSlice.actions;
+export default editMenuSlice.reducer;
---
+++ src/features/editMenu/thunks.js
@@ -0,0 +1,54 @@
+import {createAsyncThunk} from "@reduxjs/toolkit";
+import {BE_API, fetchData} from "utils/fetch";
+
+export const fetchGetMenuItemsByCompanyId = createAsyncThunk(
+    'editMenu/fetchGetMenuItemsByCompanyId',
+    async (companyId, {rejectWithValue}) => {
+        try {
+            const response = await fetchData(BE_API.MENU_ITEM.GET_BY_COMPANY_ID(companyId))
+            return response.body
+        } catch (e) {
+            return rejectWithValue(e);
+        }
+    }
+);
+
+export const fetchPostMenuItem = createAsyncThunk(
+    'editMenu/fetchPostMenuItem',
+    async (menuItem, {rejectWithValue}) => {
+        try {
+            const responce = await fetchData(BE_API.MENU_ITEM.POST_CREATE(), menuItem)
+            return responce.body
+        } catch (e) {
+            return rejectWithValue(e);
+        }
+
+    }
+);
+
+export const fetchPutMenuItem = createAsyncThunk(
+    'editMenu/fetchPutMenuItem',
+    async (menuItem, {rejectWithValue}) => {
+        try {
+            await fetchData(BE_API.MENU_ITEM.PUT_UPDATE(), menuItem)
+            return menuItem
+        } catch (e) {
+            return rejectWithValue(e);
+        }
+
+    }
+);
+
+export const fetchDeleteMenuItem = createAsyncThunk(
+    'editMenu/fetchDeleteMenuItem',
+    async (menuItemId, {rejectWithValue}) => {
+        try {
+            await fetchData(BE_API.MENU_ITEM.DELETE(), {method: 'delete', id: menuItemId})
+            return menuItemId;
+        } catch (e) {
+            debugger
+            return rejectWithValue(e);
+        }
+
+    }
+)
