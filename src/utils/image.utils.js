2088f3d38b85e20254ae5e592a20f24d2cff02bb
svladiko <vlad.serhiychuk@gmail.com>
2024-01-24 05:29:42 +0200

    Corrected company image

--- src/utils/image.utils.js
+++ src/utils/image.utils.js
@@ -4,7 +4,7 @@
 const ImageUrlFormatter = {
     formatForCompany(url) {
         // return url.replace('upload/', 'upload/b_auto,c_fill_pad,g_auto,w_375/');
-        return url.replace('upload/', 'upload/b_auto,c_fill_pad,g_auto,h_420,w_560/');
+        return url.replace('upload/', 'upload/b_auto,c_fill_pad,g_auto,h_370,w_540/');
     },
     formatForMenuItemBig(url) {
         return url.replace('upload/', 'upload/b_auto,c_fill_pad,g_auto,h_280,w_360/');
