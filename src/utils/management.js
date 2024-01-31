04584fd4a5f206718fa1b08ab1fd7a65172f95ff
svladiko <vlad.serhiychuk@gmail.com>
2024-01-31 20:09:30 +0200

    [DEP-826] FE Fix clear local storage on reset project version

--- src/utils/management.js
+++ src/utils/management.js
@@ -1,11 +1,12 @@
 import {LOCAL_STORAGE_KEY, LocalStorage} from "./localStorage";
 import {DEFAULT_LANGUAGE, getCurrentLanguage} from "./translation";
 import packageJson from "../../package.json";
+import {URL} from "./config";
 
 /**
  * Logic which clean old local storage.
  */
-export const updateLocalStorage = () => {
+export const updateProjectVersionLocalStorage = () => {
     const siteLanguage = getCurrentLanguage() || DEFAULT_LANGUAGE;
     const isShowIntro = LocalStorage.get(LOCAL_STORAGE_KEY.SHOW_INTRO);
 
@@ -22,8 +23,18 @@ export const updateLocalStorage = () => {
 export const checkUpdates = () => {
     const versionFromLocalStorage = LocalStorage.get(LOCAL_STORAGE_KEY.PROJECT_VERSION);
     if (packageJson.version !== versionFromLocalStorage) {
-        updateLocalStorage()
+        updateProjectVersionLocalStorage()
     }
+}
+
+export const resetSettings = () => {
+    localStorage.clear()
+    window.location.href = window.location.origin
+}
+
+export const resetProjectVersion = () => {
+    localStorage.clear()
+    window.location.href = window.location.origin + URL.PROJECT_UPDATED;
 }
 
 
