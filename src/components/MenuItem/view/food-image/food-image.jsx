f0dbc613c2ee92ebc7ce24bf284909a7798fcb0f
Pavlo Serhiichuk <pavel.serhiichuk@gmail.com>
2024-03-06 15:21:20 +0200

    reset wrong rebase for PMA-815

--- src/components/MenuItem/view/food-image/food-image.jsx
+++ src/components/MenuItem/view/food-image/food-image.jsx
@@ -1,26 +1,26 @@
-import {useState} from "react";
-import {useSearchParams} from "react-router-dom";
-
 import {Wrapper, LoadingWrapper} from "./food-image.style";
+
 import {ReactComponent as LoadingIcon} from "assets/icons/spinner.svg";
+
 import ImageUrlFormatter from "utils/image.utils";
+import {useState} from "react";
 
 const FoodImage = ({item}) => {
     const [isShowLoader, setIsShowLoader] = useState(true)
-    const isShowAllImages = useSearchParams()[0].get('isShowAllImages') === 'true'
 
-    if (item.isImageVisible || isShowAllImages) {
-        return (
-            <Wrapper>
-                <img onLoad={() => setIsShowLoader(false)} src={ImageUrlFormatter.formatForMenuItemBig(item.imageUrl)}
-                     alt="food image" />
-                {isShowLoader && <LoadingWrapper className='animated_svg'>
-                    <LoadingIcon />
-                </LoadingWrapper>
-                }
-            </Wrapper>
-        )
+    if (!item.isImageVisible) {
+        return;
     }
+
+    return (
+        <Wrapper>
+            <img onLoad={() => setIsShowLoader(false)} src={ImageUrlFormatter.formatForMenuItemBig(item.imageUrl)} alt="food image"/>
+            {isShowLoader && <LoadingWrapper className='animated_svg'>
+                <LoadingIcon/>
+            </LoadingWrapper>
+            }
+        </Wrapper>
+    )
 }
 
 export default FoodImage;
