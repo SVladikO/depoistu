b3c24efbb31c1c364e8e26c2a526f33d6970f785
svladiko <vlad.serhiychuk@gmail.com>
2024-04-27 23:09:35 +0300

    DEP-851 FE Connect favorite page to header icon

--- src/components/NavigationHeader/NavigationHeader.js
+++ src/components/NavigationHeader/NavigationHeader.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
+import {Link} from "react-router-dom";
 
 import {Wrapper, RightSubWrapper, CurrentLanguage} from "./NavigationHeader.style";
 
 import {ReactComponent as HeaderLogoIcon} from "assets/icons/h_logo.svg";
 import {ReactComponent as FavoriteIcon} from "assets/icons/heart1.svg";
-import {openLanguagePopup} from "../../features/language/languageSlice";
-import {translate, TRANSLATION as TR} from "../../utils/translation";
+import {openLanguagePopup} from "features/language/languageSlice";
+import {translate, TRANSLATION as TR} from "utils/translation";
+import {ROUTER} from "utils/config";
 
 const NavigationHeader = (props) => {
     const dispatch = useDispatch();
 
     return (
         <Wrapper className='pm-NavigationHeader'>
-               <HeaderLogoIcon />
-               <RightSubWrapper>
-                   <CurrentLanguage onClick={() => dispatch(openLanguagePopup())}>
-                       {translate(TR.PAGE.SETTINGS.MENU_ROW.HEADER_LANGUAGE)}
-                   </CurrentLanguage>
-                   <FavoriteIcon />
-               </RightSubWrapper>
+            <HeaderLogoIcon/>
+            <RightSubWrapper>
+                <CurrentLanguage onClick={() => dispatch(openLanguagePopup())}>
+                    {translate(TR.PAGE.SETTINGS.MENU_ROW.HEADER_LANGUAGE)}
+                </CurrentLanguage>
+                <Link to={ROUTER.FAVORITE.URL}>
+                    <FavoriteIcon/>
+                </Link>
+            </RightSubWrapper>
         </Wrapper>
     );
 };
