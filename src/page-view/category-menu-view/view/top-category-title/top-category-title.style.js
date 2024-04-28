5c33a91edfe18b126016f8d330d02d2f969b856e
svladiko <vlad.serhiychuk@gmail.com>
2024-04-28 17:01:39 +0300

    Fixed category row bg

--- src/page-view/category-menu-view/view/top-category-title/top-category-title.style.js
+++ src/page-view/category-menu-view/view/top-category-title/top-category-title.style.js
@@ -5,5 +5,6 @@ export const Wrapper = styled.div`
   height: ${p => p.isHidden ? '0px' : 'auto'};
   padding: ${p => p.isHidden ? '0px' : '18px 0 16px 16px'};
   color: ${hexToRgbA(COLOR.ACCENT1, 0.7)};
+  background: ${hexToRgbA(COLOR.ACCENT9, 0.5)};
   ${FONT.SIZE_18};
 `;
